00c0ba704ad5bb6f0a74be16dc113c43e8771571
kkmane <kk@dff.org.in>
2015-12-14 10:58:17 +0530

    start work on postgresql connection and creating tables

--- gkcore/models/meta.py
+++ gkcore/models/meta.py
@@ -6,7 +6,13 @@ from sqlalchemy.orm import (
     )
 
 from zope.sqlalchemy import ZopeTransactionExtension
+from sqlalchemy.engine import create_engine
 
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
+def dbconnect():
+	stmt = 'postgresql+psycopg2://gkadmin:gk123@localhost:5432/gnukhata'
+#now we will create an engine instance to connect to the given database.
+	engine = create_engine(stmt, echo=False)
+	return engine
