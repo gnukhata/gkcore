b0ff05e9ab6a6a208b5b65e20ade677ec13f5dae
ishan masdekar <imasdekar@dff.org.in>
2016-01-04 16:56:57 +0530

    Completed the api for organisation

--- gkcore/views/api_organisation.py
+++ gkcore/views/api_organisation.py
@@ -16,7 +16,7 @@ class api_organisation(object):
 		result = con.execute(select([gkdb.organisation.c.orgcode, gkdb.organisation.c.orgname, gkdb.organisation.c.orgtype,gkdb.organisation.c.yearstart,gkdb.organisation.c.yearend]))
 		orgs = []
 		for row in result:
-			orgs.append({"orgcode":row["orgcode"], "orgname":row["orgname"], "orgtype":row["orgtype"]	})
+			orgs.append({"orgcode":row["orgcode"], "orgname":row["orgname"], "orgtype":row["orgtype"], "yearstart":row["yearstart"], "yearend":row["yearend"]})
 		print orgs
 		return orgs
 		
@@ -28,8 +28,8 @@ class api_organisation(object):
 		print result
 		return True	
 	
-	def get(self, orgcode):
-		result = con.execute(select([gkdb.organisation]).where(gkdb.organisation.c.orgcode==orgcode))
+	def get(self,):
+		result = con.execute(select([gkdb.organisation]).where(gkdb.organisation.c.orgcode==self.request.matchdict["orgcode"]))
 		orgDetails = []
 		row = result.fetchone()
 		orgDetails.append({"orgcity":row["orgcity"], "orgaddr":row["orgaddr"], "orgpincode":row["orgpincode"], "orgstate":row["orgstate"], "orgcountry":row["orgcountry"], "orgtelno":row["orgtelno"], "orgfax":row["orgfax"], "orgwebsite":row["orgwebsite"], "orgemail":row["orgemail"], "orgpan":row["orgpan"], "orgmvat":row["orgmvat"], "orgstax":row["orgstax"], "orgregno":row["orgregno"], "orgregdate":row["orgregdate"], "orgfcrano":row["orgfcrano"], "orgfcradate":row["orgfcradate"], "roflag":row["roflag"], "booksclosedflag":row["booksclosedflag"]	})
