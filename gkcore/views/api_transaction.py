08e92e4a70fe2e959c46c8f50018d3c191961003
kkmane <kk@dff.org.in>
2016-03-16 21:06:56 +0530

    added first find criteria for find voucher as voucher type.

--- gkcore/views/api_transaction.py
+++ gkcore/views/api_transaction.py
@@ -104,3 +104,41 @@ class api_transaction(object):
 				return {"gkstatus":enumdict["Success"], "gkresult":voucher}
 			except:
 				return {"gkstatus":enumdict["ConnectionFailed"]}
+	@view_config(request_method='GET',request_param='searchby=type', renderer='json')
+	def searchByType(self):
+		try:
+			token = self.request.headers["gktoken"]
+		except:
+			return {"gkstatus": enumdict["UnauthorisedAccess"]}
+		authDetails = authCheck(token)
+		if authDetails['auth'] == False:
+			return {"gkstatus":enumdict["UnauthorisedAccess"]}
+		else:
+			try:
+				voucherType = self.request.params["vouchertype"]
+				vouchersData = con.execute(select([vouchers]).where(and_(vouchers.c.orgcode == authDetails['orgcode'],vouchers.c.vouchertype==voucherType)))
+				voucherRecords = []
+				
+				for voucher in vouchersData:
+					rawDr = dict(voucher["drs"])
+					rawCr = dict(voucher["crs"])
+					finalDR = {}
+					finalCR = {}	
+					for d in rawDr.keys():
+						accname = con.execute(select([accounts.c.accountname]).where(accounts.c.accountcode==int(d)))
+						account = accname.fetchone()
+						finalDR[account["accountname"]] = rawDr[d]
+					print finalDR
+					
+					for c in rawCr.keys():
+						accname = con.execute(select([accounts.c.accountname]).where(accounts.c.accountcode==int(c)))
+						account = accname.fetchone()
+						finalCR[account["accountname"]] = rawCr[c]
+					print finalCR
+					
+					voucherRecords.append({"vouchercode":voucher["vouchercode"],"vouchernumber":voucher["vouchernumber"],"voucherdate":str(voucher["voucherdate"]),"entrydate":str(voucher["entrydate"]),"narration":voucher["narration"],"drs":finalDR,"crs":finalCR,"prjdrs":voucher["prjdrs"],"prjcrs":voucher["prjcrs"],"vouchertype":voucher["vouchertype"],"delflag":voucher["delflag"],"orgcode":voucher["orgcode"]})
+				return {"gkstatus":enumdict["Success"],"gkresult":voucherRecords}
+			except:
+				return {"gkstatus":enumdict["ConnectionFailed"]}
+
+				
