2b93d9f6377d9b071e2c252022773a4a4afc71fa
Prajkta_Patkar <prajkta.patkar007@gmail.com>
2016-10-18 12:43:58 +0530

    Correction in transfer note.

--- gkcore/views/api_transfernote.py
+++ gkcore/views/api_transfernote.py
@@ -40,87 +40,91 @@ import gkcore
 from gkcore.models.meta import dbconnect
 
 @view_defaults(route_name='transfernote')
-class api_organisation(object):
-	def __init__(self,request):
-		self.request = Request
-		self.request = request
-		self.con = Connection
-
-		@view_config(request_method='POST',renderer='json')
-		def createtn(self):
-			try:
-				token = self.request.headers["gktoken"]
-			except:
-				return  {"gkstatus":  gkcore.enumdict["UnauthorisedAccess"]}
-				authDetails = authCheck(token)
-			if authDetails["auth"] == False:
-				return  {"gkstatus":  enumdict["UnauthorisedAccess"]}
-			else:
-				try:
-					self.con = eng.connect()
-					dataset = self.request.json_body
-					dataset["orgcode"] = authDetails["orgcode"]
-					result = self.con.execute(transfernote.insert(),[dataset])
-					return {"gkstatus":enumdict["Success"]}
-				except exc.IntegrityError:
-					return {"gkstatus":enumdict["DuplicateEntry"]}
-				except:
-					return {"gkstatus":enumdict["ConnectionFailed"]}
-				finally:
-					self.con.close()
+class api_transfernote(object):
+    def __init__(self,request):
+        self.request = Request
+        self.request = request
+        self.con = Connection
+        
+    @view_config(request_method='POST',renderer='json')
+    def createtn(self):
+        try:
+            token = self.request.headers["gktoken"]
+        except:
+            return  {"gkstatus":  gkcore.enumdict["UnauthorisedAccess"]}
+        authDetails = authCheck(token)
+        if authDetails["auth"] == False:
+            return  {"gkstatus":  enumdict["UnauthorisedAccess"]}
+        else:
+            try:
+                self.con = eng.connect()
+                dataset = self.request.json_body
+                dataset["orgcode"] = authDetails["orgcode"]
+                result = self.con.execute(transfernote.insert(),[dataset])
+                return {"gkstatus":enumdict["Success"]}
+            except exc.IntegrityError:
+                return {"gkstatus":enumdict["DuplicateEntry"]}
+            except:
+                return {"gkstatus":enumdict["ConnectionFailed"]}
+            finally:
+                self.con.close()
+                
+    @view_config(request_param="tn=all",request_method='GET',renderer='json')
+    def getAllTransferNote(self):
+        try:
+            token = self.request.headers["gktoken"]
+        except:
+            return  {"gkstatus":  enumdict["UnauthorisedAccess"]}
+        authDetails = authCheck(token)
+        if authDetails["auth"] == False:
+            return {"gkstatus":enumdict["UnauthorisedAccess"]}
+        else:
+            try:
+                self.con = eng.connect()
+                result = self.con.execute(select([transfernote.c.transfernoteno, transfernote.c.transfernotedate,transfernote.c.transportationmode,transfernote.c.productdetails,transfernote.c.nopkt, transfernote.c.recieved,transfernote.c.fromgodown,transfernote.c.togodown,transfernote.c.orgcode]).order_by(transfernote.c.transfernotedate))
+                transfernote = []
+                for row in result:
+                    tn.append({"TransferNote no": row["transfernoteno"], "TransferNote Date":row["transfernotedate"] , "Transportation Mode":row["transportationmode"], "Product Details": row["productdetails"], "No. of packets": row["nopkt"], "Recieved": row["recieved"], "From godown": row["fromgodown"], "To godown": row["togodown"], "OrgCode": row["orgcode"] })
+                self.con.close()
+                return {"gkstatus":enumdict["Success"], "gkdata":transfernote}
+            except:
+                self.con.close()
+                return {"gkstatus":enumdict["ConnectionFailed"]}
 				
-				
-					@view_config(request_param="tn=all",request_method='GET',renderer='json')
-					def getAllTn(self):
-						try:
-							token = self.request.headers["gktoken"]
-						except:
-							return  {"gkstatus":  enumdict["UnauthorisedAccess"]}
-							authDetails = authCheck(token)
-							if authDetails["auth"] == False:
-								return {"gkstatus":enumdict["UnauthorisedAccess"]}
-							else:
-								try:
-									self.con = eng.connect()
-									result = self.con.execute(select([transfernote.c.transfernoteno, transfernote.c.transfernotedate,transfernote.c.transportationmode,transfernote.c.productdetails,transfernote.c.nopkt, transfernote.c.recieved,transfernote.c.fromgodown,transfernote.c.togodown,transfernote.c.orgcode]).order_by(transfernote.c.transfernotedate))
-									transfernote = []
-									for row in result:
-										products.append({"TransferNote no": row["transfernoteno"], "TransferNote Date":row["transfernotedate"] , "Transportation Mode":row["transportationmode"], "Product Details": row["productdetails"], "No. of packets": row["nopkt"], "Recieved": row["recieved"], "From godown": row["fromgodown"], "To godown": row["togodown"], "OrgCode": row["orgcode"] })
-										self.con.close()
-										return {
-											"gkstatus":enumdict["Success"], "gkdata":transfernote}
-								except:
-									self.con.close()
-									return {"gkstatus":enumdict["ConnectionFailed"]}
-				
-				
-				
+                
+    @view_config(request_method='DELETE', renderer ='json')
+    def deleteTransferNote(self):
+        try:
+            token = self.request.headers["gktoken"]
+        except:
+            return  {"gkstatus":  enumdict["UnauthorisedAccess"]}
+        authDetails = authCheck(token)
+        if authDetails["auth"]==False:
+            return {"gkstatus":enumdict["UnauthorisedAccess"]}
+        else:
+            try:
+                self.con = eng.connect()
+                dataset = self.request.json_body
+                result = self.con.execute(transfernote.delete().where(transfernote.c.transfernoteno == dataset["transfernoteno"]))
+                #result = self.con.execute(stock.delete().where(and_(stock.c.dcinvid==dataset["dcid"],stock.c.dcinvflag==4)))
+                return {"gkstatus":enumdict["Success"]}
+            except exc.IntegrityError:
+                return {"gkstatus":enumdict["ActionDisallowed"]}
+            except:
+                return {"gkstatus":enumdict["ConnectionFailed"] }
+            finally:
+                self.con.close()
 
 
 
-@view_config(request_method='DELETE',renderer='json')
-def deletePurchaseOrder(self):
-		try:
-			token = self.request.headers["gktoken"]
-		except:
-			return  {"gkstatus":  enumdict["UnauthorisedAccess"]}
-		authDetails = authCheck(token)
-		if authDetails["auth"]==False:
-			return {"gkstatus":enumdict["UnauthorisedAccess"]}
-		else:
-			try:
-				self.con = eng.connect()
-				dataset = self.request.json_body
-				result = self.con.execute(transfernote.delete().where(transfernote.c.transfernoteno == dataset["transfernoteno"]))
-				return {"gkstatus":enumdict["Success"]}
-			except exc.IntegrityError:
-				return {"gkstatus":enumdict["ActionDisallowed"]}
-			except:
-				return {"gkstatus":enumdict["ConnectionFailed"] }
-			finally:
-				self.con.close()
 
-		 
+
+
+
+
+
+
+
 
 	
 	   
