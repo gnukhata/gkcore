930b76d1fa6d1d75279222615517dc788b683157
kk <kk@dff.org.in>
2016-11-07 18:34:26 +0530

    refined the code to have data only dump without create table commans.

--- gkcore/views/api_backuprestore.py
+++ gkcore/views/api_backuprestore.py
@@ -60,153 +60,21 @@ class api_backuprestore(object):
 		if authDetails["auth"] == False:
 			return  {"gkstatus":  enumdict["UnauthorisedAccess"]}
 		else:
-		#	try:
+			try:
 				self.con = eng.connect()
 				user=self.con.execute(select([users.c.userrole]).where(users.c.userid == authDetails["userid"] ))
 				userRole = user.fetchone()
-				#dataset = self.request.json_body
 				if userRole[0]==-1:
-					os.system("pg_dump gkdata -f /tmp/backup.sql")
+					os.system("pg_dump -a gkdata -f /tmp/backup.sql")
 					backupfile = open("/tmp/backup.sql","r")
 					backupdata = backupfile.read()
 					backupfile.close()
 					return {"gkstatus":enumdict["Success"],"gkdata":backupdata}
 				else:
 					return {"gkstatus":  enumdict["BadPrivilege"]}
-		#	except exc.IntegrityError:
-		#		return {"gkstatus":enumdict["DuplicateEntry"]}
-		#	except:
-		#		return {"gkstatus":gkcore.enumdict["ConnectionFailed"]}
-		#	finally:
-		#		self.con.close()
-				
-	@view_config(request_method='GET',request_param='psflag',renderer='json')
-	def getprodtax(self):
-		"""
-		This method will return the list of taxes for a product.
-		The tax will be either returned for a given product, or with the combination of product and state (Specially for VAT).
-		Takes in a parameter for productcode, optionally statecode.
-		If the flag is p then all the taxes for that product will be returned.
-		If it is s then for that product for that state will be returned.
-		returns a list of JSON objects.
-		"""
-		try:
-			print "all tax"
-			token = self.request.headers["gktoken"]
-		except:
-			return  {"gkstatus":  enumdict["UnauthorisedAccess"]}
-		authDetails = authCheck(token)
-		if authDetails["auth"] == False:
-			return {"gkstatus":enumdict["UnauthorisedAccess"]}
-		else:
-			try:
-				self.con = eng.connect()
-				if(self.request.params["psflag"]=="p"):
-					result = self.con.execute(select([tax.c.taxname,tax.c.taxrate,tax.c.state]).where(tax.c.productcode==self.request.params["productcode"]))
-					tx =[]
-					for row in result:
-						tx.append({"taxname":row["taxname"], "taxrate":"%.2f"%float(row["taxrate"]),"state":row["state"]})
-					return {"gkstatus":enumdict["Success"],"gkresult":tx}
-					
-				
-				if(self.request.params["psflag"]=="s"):
-					result = self.con.execute(select([tax.c.taxname,tax.c.taxrate]).where(and_(tax.c.productcode==self.request.params["productcode"],tax.c.state==self.request.params["state"])))
-					tx =[]
-					for row in result:
-						tx.append({"taxname":row["taxname"], "taxrate":"%.2f"%float(row["taxrate"])})
-					return {"gkstatus":enumdict["Success"], "gkresult":tx}
-					self.con.close()
-				return {"gkstatus":enumdict["Success"],}
-			except:
-				self.con.close()
-				return {"gkstatus":enumdict["ConnectionFailed"]}
-			finally:
-				self.con.close()
-
-		
-	@view_config(request_method='GET',renderer='json')
-	def getAllTax(self):
-		"""This method returns	all existing data about taxes for current organisation   """
-		try:
-			
-			token = self.request.headers["gktoken"]
-		except:
-			return  {"gkstatus":  enumdict["UnauthorisedAccess"]}
-		authDetails = authCheck(token)
-		if authDetails["auth"] == False:
-			return {"gkstatus":enumdict["UnauthorisedAccess"]}
-		else:
-			try:
-				self.con = eng.connect()
-				
-				result = self.con.execute(select([tax]).where(tax.c.orgcode==authDetails["orgcode"]))
-				tx = []
-				for row in result:
-					tx.append({"taxid": row["taxid"], "taxname":row["taxname"], "taxrate":"%.2f"%float(row["taxrate"]),"state":row["state"], "categorycode": row["categorycode"], "productcode": row["productcode"], "orgcode": row["orgcode"] })
-					
-				self.con.close()
-				return {"gkstatus":enumdict["Success"], "gkdata":tx}
-			except:
-				self.con.close()
-				return {"gkstatus":enumdict["ConnectionFailed"]}
-			finally:
-				self.con.close()
-				
-				
-	@view_config(request_method='PUT', renderer='json')
-	def edittaxdata(self):
-		"""  This method updates the taxdata				   """
-		try:
-			token = self.request.headers["gktoken"]
-		except:
-			return  {"gkstatus":  gkcore.enumdict["UnauthorisedAccess"]}
-
-		authDetails = authCheck(token)
-		if authDetails["auth"] == False:
-			return  {"gkstatus":  enumdict["UnauthorisedAccess"]}
-		else:
-			try:
-				self.con = eng.connect()
-				user=self.con.execute(select([users.c.userrole]).where(users.c.userid == authDetails["userid"] ))
-				userRole = user.fetchone()
-				dataset = self.request.json_body
-				
-				if userRole[0]==-1:
-					
-					result = self.con.execute(tax.update().where(tax.c.taxid == dataset["taxid"]).values(dataset))
-					print result
-					return {"gkstatus":enumdict["Success"]}
-				else:
-					return {"gkstatus":  enumdict["BadPrivilege"]}
-			except:
-				return {"gkstatus":gkcore.enumdict["ConnectionFailed"] }
-			finally:
-				self.con.close()
-				
-	@view_config(request_method='DELETE', renderer ='json')
-	def deletetaxdata(self):
-		"""  This method delets the tax data by matching taxid				 """
-		try:
-			token = self.request.headers["gktoken"]
-		except:
-			return  {"gkstatus":  enumdict["UnauthorisedAccess"]}
-		authDetails = authCheck(token)
-		if authDetails["auth"]==False:
-			return {"gkstatus":enumdict["UnauthorisedAccess"]}
-		else:
-			try:
-				self.con = eng.connect()
-				user=self.con.execute(select([users.c.userrole]).where(users.c.userid == authDetails["userid"] ))
-				userRole = user.fetchone()
-				dataset = self.request.json_body
-				if userRole[0]==-1:
-					result = self.con.execute(tax.delete().where(tax.c.taxid==dataset["taxid"]))
-					return {"gkstatus":enumdict["Success"]}
-				else:
-					{"gkstatus":  enumdict["BadPrivilege"]}
 			except exc.IntegrityError:
-				return {"gkstatus":enumdict["ActionDisallowed"]}
+				return {"gkstatus":enumdict["DuplicateEntry"]}
 			except:
-				return {"gkstatus":enumdict["ConnectionFailed"] }
+				return {"gkstatus":gkcore.enumdict["ConnectionFailed"]}
 			finally:
 				self.con.close()
