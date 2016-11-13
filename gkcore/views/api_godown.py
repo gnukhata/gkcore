a24467401aed6f94311058d67c7f085f28e3511c
abhi1tb <abhijithb21@openmailbox.org>
2016-11-13 14:22:36 +0530

    Godown get now returns state and serial number too

--- gkcore/views/api_godown.py
+++ gkcore/views/api_godown.py
@@ -24,6 +24,7 @@ Contributors:
 "Ishan Masdekar " <imasdekar@dff.org.in>
 "Navin Karkera" <navin@dff.org.in>
 "Bhavesh Bawadhane" <bbhavesh07@gmail.com>
+"Abhijith Balan" <abhijithb21@openmailbox.org>
 """
 
 
@@ -102,10 +103,12 @@ class api_godown(object):
 		else:
 			try:
 				self.con = eng.connect()
-				result = self.con.execute(select([godown.c.goname,godown.c.goid,godown.c.goaddr,godown.c.gocontact]).where(godown.c.orgcode==authDetails["orgcode"]).order_by(godown.c.goname))
+				result = self.con.execute(select([godown]).where(godown.c.orgcode==authDetails["orgcode"]).order_by(godown.c.goname))
 				godowns = []
+				srno=1
 				for row in result:
-					godowns.append({"goname":row["goname"], "goid":row["goid"], "goaddr":row["goaddr"],"gocontact":row["gocontact"]})
+					godowns.append({"srno":srno, "goid": row["goid"], "goname": row["goname"], "goaddr": row["goaddr"], "gocontact": row["gocontact"],"state":row["state"],"contactname":row["contactname"],"designation":row["designation"]})
+					srno = srno+1
 				return {"gkstatus": gkcore.enumdict["Success"], "gkresult":godowns }
 			except:
 				return {"gkstatus":gkcore.enumdict["ConnectionFailed"] }
