39ff82146bc98a6b31cd49432afb8e593d299a95
Vaibhav Kurhe <vaibspidy@openmailbox.org>
2016-12-14 16:01:47 +0530

    Testcases for api_user

---
+++ gkcore/tests/test_user.py
@@ -0,0 +1,80 @@
+import requests, json
+#from gkcore.views.api_login import authCheck
+"""
+Initially, setup_class() runs and at last, teardown_class() will run.
+In between these two methods, initially, setup(), then the method written e.g. test_update_user, and then teardown() will be executed.
+The sequence of these methods is decided based on alphabetical order.
+"""
+class TestUser:
+	@classmethod
+	def setup_class(self):
+		orgdata = {"orgdetails":{'orgname': 'Test Organisation', 'yearend': '2016-03-31', 'yearstart': '2015-04-01', 'orgtype': 'Profit Making', 'invflag': 1}, "userdetails":{"username":"admin", "userpassword":"admin","userquestion":"who am i?", "useranswer":"hacker"}}
+		result = requests.post("http://127.0.0.1:6543/organisations",data=json.dumps(orgdata))
+		self.key = result.json()["token"]
+		self.header={"gktoken":self.key}
+
+	@classmethod
+	def teardown_class(self):
+		result = requests.delete("http://127.0.0.1:6543/organisations", headers=self.header)
+
+	def setup(self):
+		gkdata = {"username":"demo_user","userpassword":"demo_passwd","userrole":0,"userquestion":"what is my pet name?","useranswer":"cat"}
+		result = requests.post("http://127.0.0.1:6543/users", data=json.dumps(gkdata), headers=self.header)
+		result = requests.get("http://127.0.0.1:6543/users", headers=self.header)
+		for record in result.json()["gkresult"]:
+			if record["username"] == "demo_user":
+				self.demo_userid = record["userid"]
+				break
+
+	def teardown(self):
+		gkdata={"userid":self.demo_userid}
+		result = requests.delete("http://127.0.0.1:6543/users", data=json.dumps(gkdata), headers=self.header)
+
+	def test_create_user(self):
+		gkdata = {"username":"test_user","userpassword":"test_passwd","userrole":1,"userquestion":"what is my pet name?","useranswer":"moti"}
+		result = requests.post("http://127.0.0.1:6543/users", data=json.dumps(gkdata), headers=self.header)
+		assert result.json()["gkstatus"] == 0
+
+	def test_delete_user(self):
+		result = requests.get("http://127.0.0.1:6543/users", headers=self.header)
+		for record in result.json()["gkresult"]:
+			if record["username"] == "test_user":
+				self.userid = record["userid"]
+				break
+		gkdata={"userid": self.userid}
+		result = requests.delete("http://127.0.0.1:6543/users", data=json.dumps(gkdata), headers=self.header)
+		assert result.json()["gkstatus"] == 0
+
+	def test_update_user(self):
+		gkdata={"userid":self.demo_userid, "username":"demo_user", "userpassword":"demo_passwd"}
+		result = requests.put("http://127.0.0.1:6543/users", headers=self.header, data=json.dumps(gkdata))
+		assert result.json()["gkstatus"] == 0
+
+	def test_get_all_users(self):
+		result = requests.get("http://127.0.0.1:6543/users", headers=self.header)
+		assert result.json()["gkstatus"] == 0
+
+	def test_get_single_user(self):
+		gkdata={"userid": self.demo_userid }
+		result = requests.get("http://127.0.0.1:6543/user", headers=self.header, data=json.dumps(gkdata))
+		data = result.json()["gkresult"]
+		print data
+		assert data["username"] == "admin" and data["userpassword"] == "admin" and data["userrole"] == -1 and data["userquestion"] == "who am i?" and data["useranswer"] == "hacker"
+
+	def test_verify_security_answer(self):
+		result = requests.get("http://127.0.0.1:6543/forgotpassword?type=securityanswer&userid=%s&useranswer=cat" %(self.demo_userid))
+		assert result.json()["gkstatus"] == 0
+
+	def test_update_old_password(self):
+		gkdata = {"userid":self.demo_userid,"userpassword":"demo_newpasswd","useranswer":"cat"}
+		result = requests.put("http://127.0.0.1:6543/forgotpassword", data =json.dumps(gkdata))
+		assert result.json()["gkstatus"] == 0
+
+	def test_add_theme(self):
+		themename= {"themename":"Cosmo"}
+		result= requests.put("http://127.0.0.1:6543/user?type=theme",headers=self.header,data =json.dumps(themename))
+		assert result.json()["gkstatus"] == 0
+
+	def test_get_theme(self):
+		result = requests.get("http://127.0.0.1:6543/user?type=theme",headers=self.header)
+		assert result.json()["gkresult"] == "Cosmo"
