354e2bb1281b01b09689317af6404a8e29b94ec1
Vaibhav Kurhe <vaibspidy@openmailbox.org>
2016-12-15 18:42:22 +0530

    Testcase api_transaction: test_create_and_delete_voucher() completed

--- gkcore/tests/test_transaction.py
+++ gkcore/tests/test_transaction.py
@@ -65,7 +65,7 @@ class TestTransaction:
 			if record["groupname"] == "Current Assets":
 				self.demo_groupcode = record["groupcode"]
 				break
-		result = requests.get("http://127.0.0.1:6543/groupDetails/%s"%(self.groupcode), headers=self.header)
+		result = requests.get("http://127.0.0.1:6543/groupDetails/%s"%(self.demo_groupcode), headers=self.header)
 		for record in result.json()["gkresult"]:
 			if record["subgroupname"] == "Bank":
 				self.demo_grpcode = record["groupcode"]
@@ -73,28 +73,35 @@ class TestTransaction:
 		""" Initialization of two Accounts for creating a voucher """
 		gkdata = {"accountname":"Bank Of India","openingbal":500,"groupcode":self.demo_grpcode}
 		result = requests.post("http://127.0.0.1:6543/accounts", data =json.dumps(gkdata),headers=self.header)
+		#print result.json()["gkstatus"], "India status"
 		result = requests.get("http://127.0.0.1:6543/accounts", headers=self.header)
 		for record in result.json()["gkresult"]:
 			if record["accountname"] == "Bank Of India":
+				#print result.json()["gkstatus"], "Bank of India"
 				self.demo_accountcode1 = record["accountcode"]
 				break
-		gkdata = {"accountname":"Bank of Badoda","openingbal":1000,"groupcode":self.demo_grpcode}
+		gkdata = {"accountname":"Bank Of Badoda","openingbal":1000,"groupcode":self.demo_grpcode}
 		result = requests.post("http://127.0.0.1:6543/accounts", data =json.dumps(gkdata),headers=self.header)
+		#print result.json()["gkstatus"], "Badoda status"
 		result = requests.get("http://127.0.0.1:6543/accounts", headers=self.header)
 		for record in result.json()["gkresult"]:
 			if record["accountname"] == "Bank Of Badoda":
+				#print result.json()["gkstatus"], "Bank of Badoda"
 				self.demo_accountcode2 = record["accountcode"]
 				break
-		""" Preparing gkdata to pass to Transaction POST """
+		""" Preparing gkdata to pass to Create Voucher """
 		drs = {self.demo_accountcode1: 100}
 		crs = {self.demo_accountcode2: 100}
-		gkdata={"invid": None,"vouchernumber":100,"voucherdate":2016-03-30,"narration":"Demo Narration","drs":drs,"crs":crs,"vouchertype":"purchase","projectcode":int(self.projectcode)}
+		gkdata={"invid": None,"vouchernumber":100,"voucherdate":"2016-03-30","narration":"Demo Narration","drs":drs,"crs":crs,"vouchertype":"purchase","projectcode":int(self.projectcode)}
 		result = requests.post("http://127.0.0.1:6543/transaction",data=json.dumps(gkdata) , headers=self.header)
+		#print "gkstatus: post ", result.json()["gkstatus"]
 
-		vnum = 100
-		searchby = vnum
+		vnum = "100"#string or integer?
+		searchby = "vnum"
 		result = requests.get("http://127.0.0.1:6543/transaction?searchby=%s&voucherno=%s"%(searchby,vnum), headers=self.header)
-		self.demo_vouchercode = result.json()["gkresult"]["vouchercode"]
+		#print "gkstatus: get ", result.json()["gkstatus"]
+		#print "list: ", result.json()["gkresult"]
+		self.demo_vouchercode = result.json()["gkresult"][0]["vouchercode"]
 
 	def teardown(self):
 		gkdata={"accountcode":self.demo_accountcode1}
@@ -103,3 +110,41 @@ class TestTransaction:
 		result = requests.delete("http://127.0.0.1:6543/accounts",data =json.dumps(gkdata), headers=self.header)
 		gkdata={"vouchercode": self.demo_vouchercode}
 		result = requests.delete("http://127.0.0.1:6543/transaction",data =json.dumps(gkdata), headers=self.header)
+
+	def test_create_and_delete_voucher(self):
+		""" Create and Delete Voucher Code """
+		""" Create voucher """
+		""" Initialization of two Accounts for creating a voucher """
+		gkdata = {"accountname":"India Bank","openingbal":500,"groupcode":self.demo_grpcode}
+		result = requests.post("http://127.0.0.1:6543/accounts", data =json.dumps(gkdata),headers=self.header)
+		result = requests.get("http://127.0.0.1:6543/accounts", headers=self.header)
+		for record in result.json()["gkresult"]:
+			if record["accountname"] == "India Bank":
+				self.accountcode1 = record["accountcode"]
+				break
+		gkdata = {"accountname":"Badoda Bank","openingbal":1000,"groupcode":self.demo_grpcode}
+		result = requests.post("http://127.0.0.1:6543/accounts", data =json.dumps(gkdata),headers=self.header)
+		result = requests.get("http://127.0.0.1:6543/accounts", headers=self.header)
+		for record in result.json()["gkresult"]:
+			if record["accountname"] == "Badoda Bank":
+				self.accountcode2 = record["accountcode"]
+				break
+		drs = {self.accountcode1: 200}
+		crs = {self.accountcode2: 200}
+		self.vouchernumber = 111
+		gkdata={"invid": None,"attachment":None,"attachmentcount":0,"vouchernumber":self.vouchernumber,"voucherdate":"2016-03-20","narration":"Test Narration","drs":drs,"crs":crs,"vouchertype":"purchase","projectcode":int(self.projectcode)}
+		result_post = requests.post("http://127.0.0.1:6543/transaction",data=json.dumps(gkdata) , headers=self.header)
+
+		""" Delete voucher """
+		gkdata={"accountcode":self.accountcode1}
+		result = requests.delete("http://127.0.0.1:6543/accounts",data =json.dumps(gkdata), headers=self.header)
+		gkdata={"accountcode":self.accountcode2}
+		result = requests.delete("http://127.0.0.1:6543/accounts",data =json.dumps(gkdata), headers=self.header)
+
+		vnum = "111"#string or integer?
+		searchby = "vnum"
+		result = requests.get("http://127.0.0.1:6543/transaction?searchby=%s&voucherno=%s"%(searchby,vnum), headers=self.header)
+		vouchercode = result.json()["gkresult"][0]["vouchercode"]
+		gkdata={"vouchercode": vouchercode}
+		result_delete = requests.delete("http://127.0.0.1:6543/transaction",data =json.dumps(gkdata), headers=self.header)
+		assert result_post.json()["gkstatus"] == 0 and result_delete.json()["gkstatus"] == 0
