7fcf00cf26d52fe39e02dc94e63a0f1b15486195
Bhavesh <bbhavesh07@gmail.com>
2016-12-16 15:07:20 +0530

    Ledger Tested

--- gkcore/tests/test_reports.py
+++ gkcore/tests/test_reports.py
@@ -53,20 +53,16 @@ class TestReports:
 		""" Initialization of two Accounts for creating a voucher """
 		gkdata = {"accountname":"Bank Of India","openingbal":500,"groupcode":self.demo_grpcode}
 		result = requests.post("http://127.0.0.1:6543/accounts", data =json.dumps(gkdata),headers=self.header)
-		#print result.json()["gkstatus"], "India status"
 		result = requests.get("http://127.0.0.1:6543/accounts", headers=self.header)
 		for record in result.json()["gkresult"]:
 			if record["accountname"] == "Bank Of India":
-				#print result.json()["gkstatus"], "Bank of India"
 				self.demo_accountcode1 = record["accountcode"]
 				break
 		gkdata = {"accountname":"Bank Of Badoda","openingbal":1000,"groupcode":self.demo_grpcode}
 		result = requests.post("http://127.0.0.1:6543/accounts", data =json.dumps(gkdata),headers=self.header)
-		#print result.json()["gkstatus"], "Badoda status"
 		result = requests.get("http://127.0.0.1:6543/accounts", headers=self.header)
 		for record in result.json()["gkresult"]:
 			if record["accountname"] == "Bank Of Badoda":
-				#print result.json()["gkstatus"], "Bank of Badoda"
 				self.demo_accountcode2 = record["accountcode"]
 				break
 		""" Preparing gkdata to pass to Create Voucher """
@@ -74,12 +70,9 @@ class TestReports:
 		crs = {self.demo_accountcode2: 100}
 		gkdata={"invid": None,"vouchernumber":100,"voucherdate":"2016-03-30","narration":"Demo Narration","drs":drs,"crs":crs,"vouchertype":"purchase","projectcode":int(self.projectcode)}
 		result = requests.post("http://127.0.0.1:6543/transaction",data=json.dumps(gkdata) , headers=self.header)
-		#print "gkstatus: post ", result.json()["gkstatus"]
-		vnum = "100"#string or integer?
+		vnum = "100"
 		searchby = "vnum"
 		result = requests.get("http://127.0.0.1:6543/transaction?searchby=%s&voucherno=%s"%(searchby,vnum), headers=self.header)
-		#print "gkstatus: get ", result.json()["gkstatus"]
-		#print "list: ", result.json()["gkresult"]
 		self.demo_vouchercode = result.json()["gkresult"][0]["vouchercode"]
 
 	@classmethod
@@ -94,6 +87,10 @@ class TestReports:
 		result = requests.delete("http://127.0.0.1:6543/accounts",data =json.dumps(gkdata), headers=self.header)
 		result = requests.delete("http://127.0.0.1:6543/organisations", headers=self.header)
 
-	def test_monthly_ledget(self):
+	def test_monthly_ledger(self):
 		result = requests.get("http://127.0.0.1:6543/report?type=monthlyledger&accountcode=%d"%(self.demo_accountcode1), headers=self.header)
+		assert result.json()["gkstatus"] == 0
+
+	def test_ledger(self):
+		result = requests.get("http://127.0.0.1:6543/report?type=ledger&accountcode=%d&calculatefrom=%s&calculateto=%s&financialstart=%s&projectcode=%d"%(self.demo_accountcode1, "2015-04-01", "2016-03-31", "2015-04-01", int(self.projectcode)), headers=self.header)
 		assert result.json()["gkstatus"] == 0
