5e8cdc91c6fa591249d170f99ffa1a52baebc3c6
Vaibhav Kurhe <vaibspidy@openmailbox.org>
2016-12-18 12:48:20 +0530

    Modified test_delchal: made some corrections in the order of deletion

--- gkcore/tests/test_delchal.py
+++ gkcore/tests/test_delchal.py
@@ -32,6 +32,8 @@ class TestDelChal:
 		self.key = result.json()["token"]
 		self.header={"gktoken":self.key}
 
+		""" User creation """
+		""" Check and delete the user creation if it's not being used! """
 		userdata = {"username":"user","userpassword":"passwd","userrole":0,"userquestion":"what is my pet name?","useranswer":"cat"}
 		result = requests.post("http://127.0.0.1:6543/users", data=json.dumps(userdata), headers=self.header)
 		result = requests.get("http://127.0.0.1:6543/users", headers=self.header)
@@ -39,7 +41,9 @@ class TestDelChal:
 			if record["username"] == "user":
 				self.userid = record["userid"]
 				break
-		""" Doubt: This should be a customer or supplier? """
+
+		""" Customer and Supplier creation """
+		""" Doubt: This should be a customer or supplier? I have created both!"""
 		custdata = {"custname":"customer","custaddr":"goregaon","custphone":"22432123","custemail":"customer@gmail.com","custfax":"FAXCUST212345","state":"Maharashtra","custpan":"CUSTPAN1234","custtan":"CUSTTAN1234","csflag":3}
 		result = requests.post("http://127.0.0.1:6543/customersupplier",data=json.dumps(custdata),headers=self.header)
 		result = requests.get("http://127.0.0.1:6543/customersupplier?qty=custall", headers=self.header)
@@ -57,24 +61,29 @@ class TestDelChal:
 				self.supid = record["custid"]
 				break
 
-		""" setup() combined with setup_class()"""
-		""" Product generation code """
+		""" setup() is also combined with setup_class()"""
+		""" Product Creation """
 		categorydata = {"categoryname":"Test Category", "subcategoryof": None}
 		result = requests.post("http://127.0.0.1:6543/categories",data=json.dumps(categorydata) ,headers=self.header)
+		print "categories creation: ", result.json()["gkstatus"]
 		result = requests.get("http://127.0.0.1:6543/categories", headers=self.header)
 		for record in result.json()["gkresult"]:
 			if record["categoryname"] == "Test Category":
 				self.democategorycode = record["categorycode"]
 				break
+
 		uomdata = {"unitname":"kilogram"}
 		result = requests.post("http://127.0.0.1:6543/unitofmeasurement", data = json.dumps(uomdata), headers=self.header)
+		print "unitofmeasurement creation: ", result.json()["gkstatus"]
 		result = requests.get("http://127.0.0.1:6543/unitofmeasurement?qty=all", headers=self.header)
 		for record in result.json()["gkresult"]:
 			if record["unitname"] == "kilogram":
 				self.demouomid = record["uomid"]
 				break
+
 		specdata= {"attrname":"Type","attrtype":0,"categorycode":self.democategorycode}
 		specresult = requests.post("http://127.0.0.1:6543/categoryspecs",data=json.dumps(specdata) ,headers=self.header)
+		print "category-specs creation: ", result.json()["gkstatus"]
 		result = requests.get("http://127.0.0.1:6543/categoryspecs?categorycode=%d"%(int(self.democategorycode)), headers=self.header)
 		for record in result.json()["gkresult"]:
 			if record["attrname"] == "Type":
@@ -84,20 +93,19 @@ class TestDelChal:
 		proddetails = {"productdesc":"Sugar","specs":{self.demospeccode: "Pure"}, "uomid":self.demouomid, "categorycode": self.democategorycode}
 		productdetails = {"productdetails":proddetails, "godetails":None, "godownflag":False}
 		result = requests.post("http://127.0.0.1:6543/products", data=json.dumps(productdetails),headers=self.header)
-		print "product creation gkstatus: ", result.json()["gkstatus"]
+		print "product creation: ", result.json()["gkstatus"]
 		self.demoproductcode = result.json()["gkresult"]
 
 		""" Creating Delchallan """
 		""" In this testcase, godown is not linked with Delivery Challan """
 		self.qty = 1
 		products = {self.demoproductcode: self.qty}
-
 		delchaldata = {"custid":self.custid,"dcno":"15","dcdate":"2016-03-30","dcflag":3}
-		""" inout=9 means IN and inout=15 means OUT """
+		""" inout = 9 means stock is IN and inout = 15 means stock is OUT """
 		stockdata = {"inout": 9, "items": products}
-		self.demo_delchalwholedata = {"delchaldata":delchaldata,"stockdata":stockdata}
-		result=requests.post("http://127.0.0.1:6543/delchal",data=json.dumps(self.demo_delchalwholedata),headers=self.header)
-		print "delchal creation gkstatus: ", result.json()["gkstatus"]
+		self.demo_delchalwholedata = {"delchaldata": delchaldata, "stockdata": stockdata}
+		result = requests.post("http://127.0.0.1:6543/delchal",data=json.dumps(self.demo_delchalwholedata),headers=self.header)
+		print  "delchal creation: ", result.json()["gkstatus"]
 		result = requests.get("http://127.0.0.1:6543/delchal?delchal=all", headers=self.header)
 		for record in result.json()["gkresult"]:
 			if record["dcno"] == "15":
@@ -109,28 +117,35 @@ class TestDelChal:
 		""" Actually no need to do all this before deleting an organisation. Since, organisation can be deleted directly which deltes all the data underneath it. Still we have done."""
 		deldata = {"dcid": self.demo_delchalid,"cancelflag": 1}
 		result = requests.delete("http://127.0.0.1:6543/delchal",data=json.dumps(deldata), headers=self.header)
+		print "delchal delete: ", result.json()["gkstatus"]
 		result = requests.delete("http://127.0.0.1:6543/products", data=json.dumps({"productcode":int(self.demoproductcode)}),headers=self.header)
+		print "products delete: ", result.json()["gkstatus"]
 		result = requests.delete("http://127.0.0.1:6543/categoryspecs",data=json.dumps({"spcode": int(self.demospeccode)}) ,headers=self.header)
-		result = requests.delete("http://127.0.0.1:6543/unitofmeasurement", data = json.dumps({"uomid":self.demouomid}), headers=self.header)
+		print "categoryspecs delete: ", result.json()["gkstatus"]
 		gkdata = {"categorycode": self.democategorycode}
 		result = requests.delete("http://127.0.0.1:6543/categories", data =json.dumps(gkdata), headers=self.header)
-
+		print "categories delete: ", result.json()["gkstatus"]
 		result = requests.delete("http://127.0.0.1:6543/organisations", headers=self.header)
+		print "organisations delete: ", result.json()["gkstatus"]
+		result = requests.delete("http://127.0.0.1:6543/unitofmeasurement", data = json.dumps({"uomid":self.demouomid}), headers=self.header)
+		print "unitofmeasurement delete: ", result.json()["gkstatus"]
 
 	def test_create_and_delete_delchal(self):
-		qty = 2
+		""" Create and Delete Delivery Challan """
 		"""
 			IMP Doubt: In this testcase, godown is not linked with Delivery Challan
 			So, how it gets the quantity? and how and where it gets stored in the database table stock?
 		"""
+		""" Create Delivery Challan """
+		qty = 2
 		products = {self.demoproductcode: qty}
-		delchaldata = {"custid":self.custid,"dcno":"30","dcdate":"2016-03-10","dcflag":3}
-		""" inout=9 means IN and inout=15 means OUT """
+		delchaldata = {"custid": self.custid, "dcno": "30", "dcdate": "2016-03-10", "dcflag": 3}
+		""" 'inout = 9' means stock is IN and 'inout = 15' means stock is OUT """
 		stockdata = {"inout": 9, "items": products}
 		delchalwholedata = {"delchaldata":delchaldata,"stockdata":stockdata}
-		result=requests.post("http://127.0.0.1:6543/delchal",data=json.dumps(delchalwholedata),headers=self.header)
+		result = requests.post("http://127.0.0.1:6543/delchal",data=json.dumps(delchalwholedata),headers=self.header)
 
-		""" Delete Delchallan Code """
+		""" Delete Delivery Challan """
 		delchals = requests.get("http://127.0.0.1:6543/delchal?delchal=all", headers=self.header)
 		for record in delchals.json()["gkresult"]:
 			if record["dcno"] == "30":
