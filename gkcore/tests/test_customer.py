a3ee9a22c85db9380ab3abbe9fef0e3d778f551f
Vaibhav Kurhe <vaibspidy@openmailbox.org>
2016-12-18 11:18:05 +0530

    Minor change in test_customer

--- gkcore/tests/test_customer.py
+++ gkcore/tests/test_customer.py
@@ -5,7 +5,7 @@ Copyright (C) 2013, 2014, 2015, 2016 Digital Freedom Foundation
   GNUKhata is Free Software; you can redistribute it and/or modify
   it under the terms of the GNU Affero General Public License as
   published by the Free Software Foundation; either version 3 of
-  the License, or (at your option) any later version.and old.stockflag = 's'
+  the License, or (at your option) any later version.
 
   GNUKhata is distributed in the hope that it will be useful, but
   WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -37,11 +37,11 @@ class TestCustomer:
 		result = requests.delete("http://127.0.0.1:6543/organisations", headers=self.header)
 
 	def setup(self):
-		custdata = {"custname":"rahul_mahi","custaddr":"goregaon","custphone":"432123","custemail":"rahulkande@gmail.com","custfax":"FAX212345","state":"Maharashtra","custpan":"IDPAN1234","custtan":"IDTAN1234","csflag":3}
+		custdata = {"custname":"rahul kande","custaddr":"goregaon","custphone":"432123","custemail":"rahulkande@gmail.com","custfax":"FAX212345","state":"Maharashtra","custpan":"IDPAN1234","custtan":"IDTAN1234","csflag":3}
 		result = requests.post("http://127.0.0.1:6543/customersupplier",data=json.dumps(custdata),headers=self.header)
 		result = requests.get("http://127.0.0.1:6543/customersupplier?qty=custall", headers=self.header)
 		for record in result.json()["gkresult"]:
-			if record["custname"] == "rahul_mahi":
+			if record["custname"] == "rahul kande":
 				self.democustid = record["custid"]
 				break
 
@@ -67,7 +67,7 @@ class TestCustomer:
 
 	def test_get_single_customer(self):
 		result = requests.get("http://127.0.0.1:6543/customersupplier?qty=single&custid=%d"%(int(self.democustid)), headers=self.header)
-		assert result.json()["gkresult"]["custname"]=="rahul_mahi" and result.json()["gkresult"]["custemail"] == "rahulkande@gmail.com"
+		assert result.json()["gkresult"]["custname"]=="rahul kande" and result.json()["gkresult"]["custemail"] == "rahulkande@gmail.com"
 
 	def test_update_customer(self):
 		custdata = {"custname":"rahul mande","custaddr":"goregaon, mumbai west","custphone":"023524","custemail":"rahulmande@gmail.com","custfax":"FAX212345","state":"Maharashtra","custpan":"IDPAN1234","custtan":"IDTAN1234","csflag":3, "custid": int(self.democustid)}
