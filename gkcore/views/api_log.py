2256bfb1d19b893f5cf0cc4cd7e86e42f50b67f3
Bhavesh <bbhavesh07@gmail.com>
2017-02-21 22:15:24 +0530

    api_log: added a method to delete a log given its id

--- gkcore/views/api_log.py
+++ gkcore/views/api_log.py
@@ -72,7 +72,7 @@ class api_log(object):
 			finally:
 				self.con.close()
 
-    @view_config(request_method='PUT', renderer='json')
+	@view_config(request_method='PUT', renderer='json')
 	def editLog(self):
 		try:
 			token = self.request.headers["gktoken"]
@@ -137,5 +137,27 @@ class api_log(object):
 				return {"gkstatus":enumdict["Success"],"gkresult":logdata}
 			except:
 				return {"gkstatus":enumdict["ConnectionFailed"]}
+			finally:
+				self.con.close()
+
+	@view_config(request_method='DELETE', renderer ='json')
+	def deleteLog(self):
+		try:
+			token = self.request.headers["gktoken"]
+		except:
+			return  {"gkstatus":  enumdict["UnauthorisedAccess"]}
+		authDetails = authCheck(token)
+		if authDetails["auth"]==False:
+			return {"gkstatus":enumdict["UnauthorisedAccess"]}
+		else:
+			try:
+				self.con = eng.connect()
+				dataset = self.request.json_body
+				result = self.con.execute(log.delete().where(log.c.logid==dataset["logid"]))
+				return {"gkstatus":enumdict["Success"]}
+			except exc.IntegrityError:
+				return {"gkstatus":enumdict["ActionDisallowed"]}
+			except:
+				return {"gkstatus":enumdict["ConnectionFailed"] }
 			finally:
 				self.con.close()
