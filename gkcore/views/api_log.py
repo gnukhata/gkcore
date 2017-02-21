a43b789f03ece61cd3299982cae4ce46d053378f
Bhavesh <bbhavesh07@gmail.com>
2017-02-21 22:08:58 +0530

    api_log: added a method to store log

---
+++ gkcore/views/api_log.py
@@ -0,0 +1,73 @@
+
+"""
+Copyright (C) 2013, 2014, 2015, 2016 Digital Freedom Foundation
+  This file is part of GNUKhata:A modular,robust and Free Accounting System.
+
+  GNUKhata is Free Software; you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation; either version 3 of
+  the License, or (at your option) any later version.
+
+  GNUKhata is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public
+  License along with GNUKhata (COPYING); if not, write to the
+  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA  02110-1301  USA59 Temple Place, Suite 330,
+
+
+Contributors:
+"Bhavesh Bawadhane" <bbhavesh07@gmail.com>
+"""
+
+from gkcore import eng, enumdict
+from gkcore.models.gkdb import log, users
+from sqlalchemy.sql import select
+import json
+from sqlalchemy.engine.base import Connection
+from sqlalchemy import and_, exc
+from pyramid.request import Request
+from pyramid.response import Response
+from pyramid.view import view_defaults,  view_config
+from datetime import datetime,date
+import jwt
+import gkcore
+from gkcore.views.api_login import authCheck
+'''
+This class basically does Log maintenance about an organisations
+whenever any activity is performed by user it is recorded in log table
+'''
+@view_defaults(route_name='log')
+class api_log(object):
+	def __init__(self,request):
+		self.request = Request
+		self.request = request
+		self.con = Connection
+
+	@view_config(request_method='POST',renderer='json')
+	def addLog(self):
+		try:
+			token = self.request.headers["gktoken"]
+		except:
+			return  {"gkstatus":  gkcore.enumdict["UnauthorisedAccess"]}
+		authDetails = authCheck(token)
+		if authDetails["auth"] == False:
+			return  {"gkstatus":  enumdict["UnauthorisedAccess"]}
+		else:
+			try:
+				self.con = eng.connect()
+				dataset = self.request.json_body
+				dataset["orgcode"] = authDetails["orgcode"]
+				dataset["userid"] = authDetails["userid"]
+				dataset["time"] = datetime.now()
+				result = self.con.execute(log.insert(),[dataset])
+				return {"gkstatus":enumdict["Success"]}
+			except exc.IntegrityError:
+				return {"gkstatus":enumdict["DuplicateEntry"]}
+			except:
+				return {"gkstatus":gkcore.enumdict["ConnectionFailed"] }
+			finally:
+				self.con.close()
