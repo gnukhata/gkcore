e33470122c87fc18e950d782871036cddbc21759
Vaibhav Kurhe <vaibhav.kurhe@gmail.com>
2017-06-01 17:21:44 +0530

    Modified create invoice to show only unbilled delivery notes in the dropdown.

--- gkcore/views/api_invoice.py
+++ gkcore/views/api_invoice.py
@@ -418,30 +418,144 @@ The bills grid calld gkresult will return a list as it's value.
             finally:
                 self.con.close()
 
-    @view_config(request_method='DELETE', renderer ='json')
-    def deleteinvoice(self):
-        try:
-            token = self.request.headers["gktoken"]
-        except:
-            return  {"gkstatus":  enumdict["UnauthorisedAccess"]}
-        authDetails = authCheck(token)
-        if authDetails["auth"]==False:
-            return {"gkstatus":enumdict["UnauthorisedAccess"]}
-        else:
-            try:
-                self.con = eng.connect()
-                dataset = self.request.json_body
-                dataset["canceldate"]=datetime.now().date()
-                result = self.con.execute(invoice.update().where(invoice.c.invid==dataset["invid"]).values(dataset))
-                if dataset["icflag"]==9:
-                    stockcancel = {"dcinvtnflag":90}
-                else:
-                    stockcancel = {"dcinvtnflag":30}
-                result = self.con.execute(stock.update().where(and_(stock.c.dcinvtnid==dataset["invid"],stock.c.dcinvtnflag==dataset["icflag"])).values(stockcancel))
-                return {"gkstatus":enumdict["Success"]}
-            except exc.IntegrityError:
-                return {"gkstatus":enumdict["ActionDisallowed"]}
-            except:
-                return {"gkstatus":enumdict["ConnectionFailed"] }
-            finally:
-                self.con.close()
+	@view_config(request_method='DELETE', renderer ='json')
+	def deleteinvoice(self):
+		try:
+			token = self.request.headers["gktoken"]
+		except:
+			return  {"gkstatus":  enumdict["UnauthorisedAccess"]}
+		authDetails = authCheck(token)
+		if authDetails["auth"]==False:
+			return {"gkstatus":enumdict["UnauthorisedAccess"]}
+		else:
+			try:
+				self.con = eng.connect()
+				dataset = self.request.json_body
+				dataset["canceldate"]=datetime.now().date()
+				result = self.con.execute(invoice.update().where(invoice.c.invid==dataset["invid"]).values(dataset))
+				if dataset["icflag"]==9:
+					stockcancel = {"dcinvtnflag":90}
+				else:
+					stockcancel = {"dcinvtnflag":30}
+				result = self.con.execute(stock.update().where(and_(stock.c.dcinvtnid==dataset["invid"],stock.c.dcinvtnflag==dataset["icflag"])).values(stockcancel))
+				return {"gkstatus":enumdict["Success"]}
+			except exc.IntegrityError:
+				return {"gkstatus":enumdict["ActionDisallowed"]}
+			except:
+				return {"gkstatus":enumdict["ConnectionFailed"] }
+			finally:
+				self.con.close()
+
+	@view_config(request_method='GET', request_param="unbilled_delnotes", renderer ='json')
+	def unbilled_delnotes(self):
+		try:
+			token = self.request.headers["gktoken"]
+		except:
+			return  {"gkstatus":  enumdict["UnauthorisedAccess"]}
+		authDetails = authCheck(token)
+		if authDetails["auth"]==False:
+			return {"gkstatus":enumdict["UnauthorisedAccess"]}
+		else:
+			try:
+				self.con = eng.connect()
+				orgcode = authDetails["orgcode"]
+				dataset = self.request.json_body
+				inputdate = dataset["inputdate"]
+				new_inputdate = dataset["inputdate"]
+				new_inputdate = datetime.strptime(new_inputdate, "%Y-%m-%d")
+				dc_unbilled = []
+				alldcids = self.con.execute(select([delchal.c.dcid, delchal.c.dcdate]).distinct().where(and_(delchal.c.orgcode == orgcode, delchal.c.dcdate <= new_inputdate, stock.c.orgcode == orgcode, stock.c.dcinvtnflag == 4, delchal.c.dcid == stock.c.dcinvtnid)).order_by(delchal.c.dcdate))
+				alldcids = alldcids.fetchall()
+				dcResult = []
+				i = 0
+				while(i < len(alldcids)):
+					dcid = alldcids[i]
+					invidresult = self.con.execute(select([dcinv.c.invid]).where(and_(dcid[0] == dcinv.c.dcid, dcinv.c.orgcode == orgcode, invoice.c.orgcode == orgcode, invoice.c.invid == dcinv.c.invid, invoice.c.invoicedate <= new_inputdate)))
+					invidresult = invidresult.fetchall()
+					if len(invidresult) == 0:
+						pass
+					else:
+						#invid's will be distinct only. So no problem to explicitly applying distinct clause.
+						dcprodresult = self.con.execute(select([stock.c.productcode, stock.c.qty]).where(and_(stock.c.orgcode == orgcode, stock.c.dcinvtnflag == 4, dcid[0] == stock.c.dcinvtnid)))
+						dcprodresult = dcprodresult.fetchall()
+						#I am assuming :productcode must be distinct. So, I haven't applied distinct construct.
+						#what if dcprodresult or invprodresult is empty?
+						invprodresult = []
+						for invid in invidresult:
+							temp = self.con.execute(select([invoice.c.contents]).where(and_(invoice.c.orgcode == orgcode, invid == invoice.c.invid)))
+							temp = temp.fetchall()
+							#Below two lines are intentionally repeated. It's not a mistake.
+							temp = temp[0]
+							temp = temp[0]
+							invprodresult.append(temp)
+						#Now we have to compare the two results: dcprodresult and invprodresult
+						#I assume that the delchal must have at most only one entry for a particular product. If not, then it's a bug and needs to be rectified.
+						#But, in case of invprodresult, there can be more than one productcodes mentioned. This is because, with one delchal, there can be many invoices linked.
+						matchedproducts = []
+						remainingproducts = {}
+						for eachitem in dcprodresult:
+						#dcprodresult is a list of tuples. eachitem is one such tuple.
+							for eachinvoice in invprodresult:
+							#invprodresult is a list of dictionaries. eachinvoice is one such dictionary.
+								for eachproductcode in eachinvoice.keys():
+									#eachitem[0] is unique. It's not repeated.
+									dcprodcode = eachitem[0]
+									if int(dcprodcode) == int(eachproductcode):#why do we need to convert these into string to compare?
+										#this means that the product in delchal matches with the product in invoice
+										#now we will check its quantity
+										invqty = eachinvoice[eachproductcode].values()[0]
+										dcqty = eachitem[1]
+										if float(dcqty) == float(invqty):#conversion of datatypes to compatible ones is very important when comparing them.
+											#this means the quantity of current individual product is matched exactly
+											matchedproducts.append(int(eachproductcode))
+										elif float(dcqty) > float(invqty):
+											#this means current invoice has not billed the whole product quantity.
+											if dcprodcode in remainingproducts.keys():
+												if float(dcqty) == (float(remainingproducts[dcprodcode]) + float(invqty)):
+													matchedproducts.append(int(eachproductcode))
+													#whether we use eachproductcode or dcprodcode, doesn't matter. Because, both values are the same here.
+													del remainingproducts[int(eachproductcode)]
+												else:
+													#It must not be the case that below addition is greater than dcqty.
+													remainingproducts[dcprodcode] = (float(remainingproducts[dcprodcode]) + float(invqty))
+											else:
+												remainingproducts.update({dcprodcode:float(invqty)})
+										else:
+											#"dcqty < invqty" should never happen.
+											# It could happen when multiple delivery chalans have only one invoice.
+											pass
+
+						#changing previous logic..
+						if len(matchedproducts) == len(dcprodresult):
+							#Now we have got the delchals, for which invoices are also sent completely.
+							alldcids.remove(dcid)
+							i-=1
+					i+=1
+					pass
+
+				for eachdcid in alldcids:
+					singledcResult = self.con.execute(select([delchal.c.dcid, delchal.c.dcno, delchal.c.dcdate, delchal.c.dcflag, customerandsupplier.c.custname, customerandsupplier.c.csflag, delchal.c.attachmentcount]).distinct().where(and_(delchal.c.orgcode == orgcode, customerandsupplier.c.orgcode == orgcode, eachdcid[0] == delchal.c.dcid, delchal.c.custid == customerandsupplier.c.custid, stock.c.dcinvtnflag == 4, eachdcid[0] == stock.c.dcinvtnid)))
+					singledcResult = singledcResult.fetchone()
+					dcResult.append(singledcResult)
+
+				temp_dict = {}
+				srno = 1
+				for row in dcResult:
+						temp_dict = {"dcid": row["dcid"], "srno": srno, "dcno":row["dcno"], "dcdate": datetime.strftime(row["dcdate"],"%d-%m-%Y"), "dcflag": row["dcflag"], "csflag": row["csflag"], "custname": row["custname"], "attachmentcount": row["attachmentcount"]}
+						if temp_dict["dcflag"] == 19:
+							#We don't have to consider sample.
+							temp_dict["dcflag"] = "Sample"
+						elif temp_dict["dcflag"]== 6:
+							#we ignore this as well
+							temp_dict["dcflag"] = "Free Replacement"
+						if temp_dict["dcflag"] != "Sample" and temp_dict["dcflag"] !="Free Replacement":
+							dc_unbilled.append(temp_dict)
+							srno += 1
+				self.con.close()
+				return {"gkstatus":enumdict["Success"], "gkresult": dc_unbilled}
+			except exc.IntegrityError:
+				return {"gkstatus":enumdict["ActionDisallowed"]}
+			except:
+				return {"gkstatus":enumdict["ConnectionFailed"] }
+			finally:
+				self.con.close()
