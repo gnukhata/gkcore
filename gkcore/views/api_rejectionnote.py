6dcfef7af4fef5bf10e20c556b33a3595f232732
Bhavesh <bbhavesh07@gmail.com>
2017-06-12 17:59:27 +0530

    added a method which will return a dictionary of all rejection notes

--- gkcore/views/api_rejectionnote.py
+++ gkcore/views/api_rejectionnote.py
@@ -45,59 +45,78 @@ class api_rejectionnote(object):
         self.request = Request
         self.request = request
         self.con = Connection
-"""
-create method for rejectionnote resource.
-stock table is also updated after rejection entry is made.
-    -rnid goes in dcinvtnid column of stock table.
-    -dcinvtnflag column will be set to 18 for rejection note entry.
-If stock table insert fails then the rejectionnote entry will be deleted.
-"""
-@view_config(request_method='POST',renderer='json')
-def addRejectionNote(self):
-    try:
-        token = self.request.headers["gktoken"]
-    except:
-        return  {"gkstatus":  gkcore.enumdict["UnauthorisedAccess"]}
-    authDetails = authCheck(token)
-    if authDetails["auth"] == False:
-        return  {"gkstatus":  enumdict["UnauthorisedAccess"]}
-    else:
+    """
+    create method for rejectionnote resource.
+    stock table is also updated after rejection entry is made.
+        -rnid goes in dcinvtnid column of stock table.
+        -dcinvtnflag column will be set to 18 for rejection note entry.
+    If stock table insert fails then the rejectionnote entry will be deleted.
+    """
+    @view_config(request_method='POST',renderer='json')
+    def addRejectionNote(self):
         try:
-            self.con = eng.connect()
-            dataset = self.request.json_body
-            rejectionnotedata = dataset["rejectionnotedata"]
-            stockdata = dataset["stockdata"]
-            rejectionnotedata["orgcode"] = authDetails["orgcode"]
-            stockdata["orgcode"] = authDetails["orgcode"]
-            rejectionnotedata["issuerid"] = authDetails["userid"]
-            result = self.con.execute(rejectionnote.insert(),[rejectionnotedata])
-            if result.rowcount==1:
-                rniddata = self.con.execute(select([rejectionnote.c.rnid,rejectionnote.c.rndate]).where(and_(rejectionnote.c.orgcode==authDetails["orgcode"],rejectionnote.c.rnno==rejectionnotedata["rnno"])))
-                rnidrow = rniddata.fetchone()
-                stockdata["dcinvtnid"] = rnidrow["rnid"]
-                stockdata["dcinvtnflag"] = 18
-                stockdata["stockdate"] = rnidrow["rndate"]
-                items = stockdata.pop("items")
-                try:
-                    for key in items.keys():
-                        stockdata["productcode"] = key
-                        stockdata["qty"] = items[key]
-                        result = self.con.execute(stock.insert(),[stockdata])
-                        if stockdata.has_key("goid"):
-                            resultgoprod = self.con.execute(select([goprod]).where(and_(goprod.c.goid == stockdata["goid"], goprod.c.productcode==key)))
-                            if resultgoprod.rowcount == 0:
-                                result = self.con.execute(goprod.insert(),[{"goid":stockdata["goid"],"productcode": key,"goopeningstock":0.00, "orgcode":authDetails["orgcode"]}])
-
-                except:
-                    result = self.con.execute(stock.delete().where(and_(stock.c.dcinvtnid==rnidrow["rnid"],stock.c.dcinvtnflag==18)))
-                    result = self.con.execute(rejectionnote.delete().where(rejectionnote.c.rnid==rnidrow["rnid"]))
-                    return {"gkstatus":gkcore.enumdict["ConnectionFailed"] }
-                return {"gkstatus":enumdict["Success"]}
-            else:
-                return {"gkstatus":gkcore.enumdict["ConnectionFailed"] }
-        except exc.IntegrityError:
-            return {"gkstatus":enumdict["DuplicateEntry"]}
+            token = self.request.headers["gktoken"]
         except:
-            return {"gkstatus":gkcore.enumdict["ConnectionFailed"] }
-        finally:
-            self.con.close()
+            return  {"gkstatus":  gkcore.enumdict["UnauthorisedAccess"]}
+        authDetails = authCheck(token)
+        if authDetails["auth"] == False:
+            return  {"gkstatus":  enumdict["UnauthorisedAccess"]}
+        else:
+            try:
+                self.con = eng.connect()
+                dataset = self.request.json_body
+                rejectionnotedata = dataset["rejectionnotedata"]
+                stockdata = dataset["stockdata"]
+                rejectionnotedata["orgcode"] = authDetails["orgcode"]
+                stockdata["orgcode"] = authDetails["orgcode"]
+                rejectionnotedata["issuerid"] = authDetails["userid"]
+                result = self.con.execute(rejectionnote.insert(),[rejectionnotedata])
+                if result.rowcount==1:
+                    rniddata = self.con.execute(select([rejectionnote.c.rnid,rejectionnote.c.rndate]).where(and_(rejectionnote.c.orgcode==authDetails["orgcode"],rejectionnote.c.rnno==rejectionnotedata["rnno"])))
+                    rnidrow = rniddata.fetchone()
+                    stockdata["dcinvtnid"] = rnidrow["rnid"]
+                    stockdata["dcinvtnflag"] = 18
+                    stockdata["stockdate"] = rnidrow["rndate"]
+                    items = stockdata.pop("items")
+                    try:
+                        for key in items.keys():
+                            stockdata["productcode"] = key
+                            stockdata["qty"] = items[key]
+                            result = self.con.execute(stock.insert(),[stockdata])
+                            if stockdata.has_key("goid"):
+                                resultgoprod = self.con.execute(select([goprod]).where(and_(goprod.c.goid == stockdata["goid"], goprod.c.productcode==key)))
+                                if resultgoprod.rowcount == 0:
+                                    result = self.con.execute(goprod.insert(),[{"goid":stockdata["goid"],"productcode": key,"goopeningstock":0.00, "orgcode":authDetails["orgcode"]}])
+
+                    except:
+                        result = self.con.execute(stock.delete().where(and_(stock.c.dcinvtnid==rnidrow["rnid"],stock.c.dcinvtnflag==18)))
+                        result = self.con.execute(rejectionnote.delete().where(rejectionnote.c.rnid==rnidrow["rnid"]))
+                        return {"gkstatus":gkcore.enumdict["ConnectionFailed"] }
+                    return {"gkstatus":enumdict["Success"]}
+                else:
+                    return {"gkstatus":gkcore.enumdict["ConnectionFailed"] }
+            except exc.IntegrityError:
+                return {"gkstatus":enumdict["DuplicateEntry"]}
+            except:
+                return {"gkstatus":gkcore.enumdict["ConnectionFailed"] }
+            finally:
+                self.con.close()
+
+    @view_config(request_method='GET',request_param="type=all", renderer ='json')
+    def getAllRejectionNotes(self):
+        try:
+            token = self.request.headers["gktoken"]
+        except:
+            return  {"gkstatus":  gkcore.enumdict["UnauthorisedAccess"]}
+        authDetails = authCheck(token)
+        if authDetails["auth"] == False:
+            return  {"gkstatus":  gkcore.enumdict["UnauthorisedAccess"]}
+        else:
+            try:
+                self.con = eng.connect()
+                result = self.con.execute(select([rejectionnote]).where(rejectionnote.c.orgcode==authDetails["orgcode"]).order_by(rejectionnote.c.rnno))
+                return {"gkstatus": gkcore.enumdict["Success"], "gkresult":result }
+            except:
+                return {"gkstatus":gkcore.enumdict["ConnectionFailed"] }
+            finally:
+                self.con.close()
