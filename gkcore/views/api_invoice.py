1bc6437d77ba42c18505d3d288f294132ec021b4
kk <kk@dff.org.in>
2017-07-18 13:10:53 +0530

    Added formatting and all missing fields including taxamount.

--- gkcore/views/api_invoice.py
+++ gkcore/views/api_invoice.py
@@ -283,16 +283,19 @@ There will be an icFlag which will determine if it's  an incrementing or decreme
                     totalAmount = 0.00
                     if int(invrow["taxflag"]) == 22:
                         taxRate =  float(invrow["tax"]["pc"])
+                        taxAmount = (taxableAmount * (taxRate/100))
                         totalAmount = taxableAmount + (taxableAmount * (taxRate/100))
                     else:
                         TaxData = calTax(7,invrow["sourcestate"],invrow["taxstate"],pc,self.con)
                         taxResult = TaxData["gkresult"]
                         taxRate = int(taxResult["taxrate"])
                         if taxResult["taxname"] == "IGST":
+                            taxAmount = (taxableAmount * (taxRate/100))
                             totalAmount = taxableAmount + (taxableAmount * (taxRate/100))
                         else:
+                            taxAmount = (taxableAmount * (taxRate/100))
                             totalAmount = taxableAmount + (taxableAmount * ((taxRate * 2)/100))
-                            
+                    invContents[pc] = {"proddesc":desc,"uom":unitofmeasurement,"qty":"%.2f"% (float(contentsData[pc][contentsData[pc].keys()[0]])),"freeqty":"%.2f"% (float(freeqtys[pc])),"priceperunit":"%.2f"% (float(contentsData[pc].keys()[0])),"discount":"%.2f"% (float(discounts[pc])),"totalAmount":"%.2f"% (float(totalAmount)),"taxname":taxResult["taxname"],"taxrate":"%.2f"% (float(taxRate)),"taxamount":"%.2f"% (float(taxAmount))}
                         
                     
                     
