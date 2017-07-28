016a9dda943c234742fa1ceca4ed2e5d9ed2dbcc
Prajkta_Patkar <prajkta.patkar007@gmail.com>
2017-07-28 18:28:37 +0530

    attachmentcount send

--- gkcore/views/api_invoice.py
+++ gkcore/views/api_invoice.py
@@ -265,6 +265,7 @@ There will be an icFlag which will determine if it's  an incrementing or decreme
                     inv["issuername"]=invrow["issuername"]
                     inv["designation"]=invrow["designation"]
                     inv["consignee"] = invrow["consignee"]
+                    inv["attachmentcount"] = invrow["attachmentcount"]
                     inOut = self.con.execute(select([stock.c.inout]).where(stock.c.dcinvtnid==self.request.params["invid"]))
                     inOutData = inOut.fetchone()
                     inv["inoutflag"] = int(inOutData["inout"])
