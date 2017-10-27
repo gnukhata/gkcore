531a240db18a8c79f8779382ca03073ce3038387
Prajkta_Patkar <prajkta.patkar007@gmail.com>
2017-10-27 15:38:31 +0530

    documentation added and  cess field added in gkupgrade

--- gkcore/models/gkdb.py
+++ gkcore/models/gkdb.py
@@ -291,9 +291,10 @@ Note that invoice is connected to a voucher.
 So the accounting part is thus connected with stock movement of that cost.
 A new json field called freeqty.
 Consignee (shipped to) is a json field which has name , address, state, statecode,gstin as keys along with its value.
-Bankdetails is a dictionary will have bankname,accountno. and ifsccode.
+Bankdetails is a dictionary will have bankname,accountno., branchname and ifsccode.
 taxstate is a destination sate.
-sourcestate is source state from where invoice is initiated
+sourcestate is source state from where invoice is initiated.
+Structure of a tax field is {productcode:taxrate}
 """
 invoice = Table('invoice',metadata,
     Column('invid',Integer,primary_key=True),
