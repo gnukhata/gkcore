e92dcbd304ae82be4617324d3b7241292d331ea4
RESHMA <bhatawadekar1reshma@gmail.com>
2017-12-14 17:51:46 +0530

    for getting custsup state code.

--- gkcore/views/api_invoice.py
+++ gkcore/views/api_invoice.py
@@ -308,7 +308,8 @@ There will be an icFlag which will determine if it's  an incrementing or decreme
                             inv["inoutflag"] = int(inOutData["inout"])
                     custandsup = self.con.execute(select([customerandsupplier.c.custname,customerandsupplier.c.state, customerandsupplier.c.custaddr, customerandsupplier.c.custtan,customerandsupplier.c.gstin, customerandsupplier.c.csflag]).where(customerandsupplier.c.custid==invrow["custid"]))
                     custData = custandsup.fetchone()
-                    custSupDetails = {"custname":custData["custname"],"custsupstate":custData["state"],"custaddr":custData["custaddr"],"csflag":custData["csflag"]}
+                    custsupstatecode = getStateCode(custData["state"],self.con)["statecode"]
+                    custSupDetails = {"custname":custData["custname"],"custsupstate":custData["state"],"custaddr":custData["custaddr"],"csflag":custData["csflag"],"custsupstatecode":custsupstatecode}
                     if custData["custtan"] != None:
                         custSupDetails["custtin"] = custData["custtan"]
                     if custData["gstin"] != None:
@@ -323,7 +324,6 @@ There will be an icFlag which will determine if it's  an incrementing or decreme
                             except:
                                 custSupDetails["custgstin"] = None
 
-                    
                     inv["custSupDetails"] = custSupDetails
                 #contents is a nested dictionary from invoice table.
                 #It contains productcode as the key with a value as a dictionary.
