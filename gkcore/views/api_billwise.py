4c7e2dac08f48420ce7c2a0dc388066d08c85d63
abhijith <abhijithb21@disroot.org>
2018-02-09 21:37:51 +0530

    Started writing a new function for unadjusted reports

--- gkcore/views/api_billwise.py
+++ gkcore/views/api_billwise.py
@@ -226,3 +226,64 @@ It will be used for creating entries in the billwise table and updating it as ne
                 return{"gkstatus":enumdict["Success"],"invoices":unAdjInvoices}
             except:
                 return{"gkstatus":enumdict["ConnectionFailed"]}
+
+    @view_config(request_method='GET',renderer='json', request_param="type=onlybills")
+    def getOnlyUnadjustedBills(self):
+        """
+        Purpose:
+        Gets the list of unadjusted receipts and invoices.
+        description:
+        first we provide it a customerandsupplier code.
+        Then get all the receipts vouchers for that customer or supplyer.
+        now the receipts are filtered on the basis of 2 conditions.
+        firstly the vouchers is unused at all.
+        secondly if it is used then it's total should not be equal to the sum of all adjusted amounts of those invoices which have been adjusted using this vouchers.
+        For this we loop through the list of vouchers.
+        at the beginning of the loop we set a qualification flag to true.
+        we will have a nested set of 2 if conditions.
+        first we check if the given vouchers is present in the billwise table or not.
+        if it is present then the secondly if conditions checks
+        if the total amountpaid in the vouchers is equal to the sum of invoices as mentioned above.
+        if this condition too is satisfied then the flag is set to false.
+        if any of this conditions fail then flag remains true.
+        finaly there will be a 3rd if condition which checks this flag.
+        if it is true then the vouchers is added to the list to be returned.
+        """
+        try:
+            token = self.request.headers["gktoken"]
+        except:
+            return  {"gkstatus":  enumdict["UnauthorisedAccess"]}
+        authDetails = authCheck(token)
+        if authDetails["auth"]==False:
+            return {"gkstatus":enumdict["UnauthorisedAccess"]}
+        else:
+            try:
+                self.con = eng.connect()
+                csid =int(self.request.params["csid"])
+                csFlag =int(self.request.params["csflag"])
+                csn = self.con.execute(select([customerandsupplier.c.custname]).where(and_(customerandsupplier.c.custid == csid,customerandsupplier.c.orgcode==authDetails["orgcode"])))
+                csName = csn.fetchone()
+                accData = self.con.execute(select([accounts.c.accountcode]).where(and_(accounts.c.accountname == csName["custname"],accounts.c.orgcode== authDetails["orgcode"])))
+                acccode = accData.fetchone()
+                if csFlag == 3:
+                    csReceiptData = self.con.execute("select vouchercode, vouchernumber, voucherdate, crs->>'%d' as amt from vouchers where crs ? '%d' and orgcode = %d and vouchertype = 'receipt'"%(acccode["accountcode"],acccode["accountcode"],authDetails["orgcode"]))
+                if csFlag == 19:
+                    csReceiptData = self.con.execute("select vouchercode, vouchernumber, voucherdate, drs->>'%d' as amt from vouchers where drs ? '%d' and orgcode = %d and vouchertype = 'payment'"%(acccode["accountcode"],acccode["accountcode"],authDetails["orgcode"]))
+                csReceipts = csReceiptData.fetchall()
+                unAdjInvoices = []
+                for rcpt in csReceipts:
+                    invs = self.con.execute(select([func.count(billwise.c.invid).label('invFound'),func.sum(billwise.c.adjamount).label("amtAdjusted")]).where(billwise.c.vouchercode == rcpt["vouchercode"]))
+                    invsData = invs.fetchone()
+                    amtadj = 0.00
+                    if int(invsData["invFound"]) > 0:
+                        amtadj = invsData["amtAdjusted"]
+                        
+                        if float(rcpt["amt"]) == float(invsData["amtAdjusted"]):
+                            continue                    
+                csInvoices = self.con.execute(select([invoice.c.invid,invoice.c.invoiceno,invoice.c.invoicedate,invoice.c.invoicetotal,invoice.c.amountpaid]).where(and_(invoice.c.custid == csid,invoice.c.invoicetotal > invoice.c.amountpaid, invoice.c.orgcode == authDetails["orgcode"])))
+                csInvoicesData = csInvoices.fetchall()
+                for inv in csInvoicesData:
+                    unAdjInvoices.append({"invid":inv["invid"],"invoiceno":inv["invoiceno"],"invoicedate":datetime.strftime(inv["invoicedate"],'%d-%m-%Y'),"invoiceamount":"%.2f"%(float(inv["invoicetotal"])),"balanceamount":"%.2f"%(float(inv["invoicetotal"]-inv["amountpaid"]))})
+                return{"gkstatus":enumdict["Success"],"invoices":unAdjInvoices}
+            except:
+                return{"gkstatus":enumdict["ConnectionFailed"]}
