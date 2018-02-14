90e25300252d68bb6b8452dc3c6a9cb8b2ab53d2
VasudhaKadge <kadge.vasudha@gmail.com>
2018-02-14 12:01:52 +0530

    Changes in variable names. Required data fetched from invoice and customerandsupplier tables.

--- gkcore/views/api_drcrnote.py
+++ gkcore/views/api_drcrnote.py
@@ -45,7 +45,7 @@ class api_drcr(object):
     @view_config(request_method='GET',request_param="drcr=single", renderer ='json')
     def getDrCrDetails(self):
         """
-        purpose: gets details of debit and credit note from it's drcrid.
+        purpose: gets details of single debit or credit note from it's drcrid.
         """
         try:
             token = self.request.headers["gktoken"]
@@ -57,22 +57,28 @@ class api_drcr(object):
         else:
             #try:
                 self.con = eng.connect()
-                result=self.con.execute(select([drcr]).where(drcr.c.drcrid==self.request.params["drcrid"]))
-                drcrrow=result.fetchone()
-                result1=self.con.execute(select([invoice]).where(invoice.c.invid==drcrrow["invid"]))
-                inv=result1.fetchone()
-                print "\n invid=="+str(inv["invid"])
-                print "result1 invoice"+str(inv)
-
-
-                result2=self.con.execute(select([customerandsupplier.c.custname,customerandsupplier.c.state,customerandsupplier.c.custaddr]).where(customerandsupplier.c.custid==inv["custid"]))
-                custdata=result2.fetchone()
-                print "\n \n ---- this is customer data "+str(custdata)
-   
-                drcrdata = {"drcrid":drcrrow["drcrid"],"taxflag":drcrrow["taxflag"],"drcrno":drcrrow["drcrno"],"drcrdate":datetime.strftime(drcrrow["drcrdate"],"%d-%m-%Y"),"dctypeflag":drcrrow["dctypeflag"],"totreduct":"%.2f"%float(drcrrow["totreduct"]),"invid":drcrrow["invid"]}
-                print "\n drcr data"+str(drcrdata)
+                resultdrcr=self.con.execute(select([drcr]).where(drcr.c.drcrid==self.request.params["drcrid"]))
+                drcrrow=resultdrcr.fetchone()
+                drcrdata = {"drcrid":drcrrow["drcrid"],"taxflag":drcrrow["taxflag"],"drcrno":drcrrow["drcrno"],"drcrdate":datetime.strftime(drcrrow["drcrdate"],"%d-%m-%Y"),"dctypeflag":drcrrow["dctypeflag"],"caseflag":drcrrow["caseflag"],"taxflag":drcrrow["taxflag"],"totreduct":"%.2f"%float(drcrrow["totreduct"]),"invid":drcrrow["invid"],"tax":drcrrow["tax"],"contents":drcrrow["contents"],"drcrref":drcrrow["drcrref"]}
                 
+                print "\n \n drcr data"+str(drcrdata)
+                
+                resultinv=self.con.execute(select([invoice]).where(invoice.c.invid==drcrrow["invid"]))
+                invrow=resultinv.fetchone()
+                invdata={"invid":invrow["invid"],"invoiceno":invrow["invoiceno"],"custid":invrow["custid"],"invoicedate":datetime.strftime(invrow["invoicedate"],"%d-%m-%Y"),"address":invrow["address"],"issuername":invrow["issuername"],"designation":invrow["designation"]}
+
+                print " \n \n invoice data"+str(invdata)                
+
+                resultcust=self.con.execute(select([customerandsupplier.c.custid,customerandsupplier.c.custname,customerandsupplier.c.state,customerandsupplier.c.custaddr,customerandsupplier.c.gstin,customerandsupplier.c.custtan]).where(customerandsupplier.c.custid==invrow["custid"]))
+                custrow=resultcust.fetchone()
+                custdata={"custid":custrow["custid"],"custname":custrow["custname"],"state":custrow["state"],"custaddr":custrow["custaddr"],"gstin":custrow["gstin"],"custtan":custrow["custtan"]}
+                
+                print "\n \n this is customer data "+str(custdata)
             #except:
                 #return {"gkstatus":gkcore.enumdict["ConnectionFailed"] }
             #finally:
                 self.con.close()
+                
+
+
+    
