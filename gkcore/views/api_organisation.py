bc11c44bdc44b5eaecd053f11d61a4ca87623c5f
Prajkta Patkar <prajkta@disroot.org>
2018-06-06 18:36:38 +0530

    changed mafalag to 1

--- gkcore/views/api_organisation.py
+++ gkcore/views/api_organisation.py
@@ -104,7 +104,7 @@ class api_organisation(object):
             self.con.execute(select([func.count(gkdb.billwise.c.billid)]))
         except:
             self.con.execute("alter table organisation add avflag integer default 1")
-            self.con.execute("alter table organisation add maflag integer default 0")
+            self.con.execute("alter table organisation add maflag integer default 1")
             self.con.execute("alter table accounts add defaultflag integer default 0")
             organisations = self.con.execute(select([gkdb.organisation.c.orgcode]))
             for orgcode in organisations:
