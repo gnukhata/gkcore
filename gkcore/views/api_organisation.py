bc7646b728465ee957a650d348e9b5f9d3bd8e95
Nitesh <nitesh.chaughule5@gmail.com>
2018-07-04 13:50:54 +0530

    Modify the code for adding system generated uqc for old users. here taking the dictionary insted of list of dictionary.

--- gkcore/views/api_organisation.py
+++ gkcore/views/api_organisation.py
@@ -75,24 +75,18 @@ class api_organisation(object):
                 self.con.execute("alter table unitofmeasurement add description text")
                 self.con.execute("alter table unitofmeasurement add sysunit integer default 0")
 
-                ''' Following list of uom, first check in database if present then updated its description and sysunit otherwise added new unit in unitofmeasurement table'''
-                
-                listofuqc = [{'BAG':'BAG'},{'BGS':'BAGS'},{'BLS':'BAILS'},{'BTL':'BOTTLES'},{'BOU':'BOU'},{'BOX':'BOXES'},{'BKL':'BUCKLES',},{'BLK':'BULK'},{'BUN':'BUNCHES'},{'BDL':'BUNDLES'},{'CAN':'CANS'},{'CTN':'CARTONS'},{'CAS':'CASES'},{'CMS':'CENTIMETER'},{'CHI':'CHEST'},{'CLS':'COILS'},{'COL':'COLLIES'},{'CRI':'CRATES'},{'CCM':'CUBIC CENTIMETER'},{'CIN':'CUBIC INCHES'},{'CBM':'CUBIC METER'},{'CQM':'CUBIC METERS'},{'CYL':'CYLINDER'},{'SDM':'DECAMETER SQUARE'},{'DAY':'DAYS'},{'DOZ':'DOZEN'},{'DRM':'DRUMS'},{'FTS':'FEET'},{'FLK':'FLASKS'},{'GMS':'GRAMS'},{'TON':'GREAT BRITAIN TON'},{'GGR':'GREAT GROSS'},{'GRS':'GROSS'},{'GYD':'GROSS YARDS'},{'HBK':'HABBUCK'},{'HKS':'HANKS'},{'HRS':'HOURS'},{'INC':'INCHES'},{'JTA':'JOTTA'},{'KGS':'KILOGRAMS'},{'KLR':'KILOLITER'},{'KME':'KILOMETERS'},{'LTR':'LITERS'},{'LOG':'LOGS'},{'LOT':'LOTS'},{'MTR':'METER'},{'MTS':'METRIC TON'},{'MGS':'MILLIGRAMS'},{'MLT':'MILLILITER'},{'MMT':'MILLIMETER'},{'NONE':'NOT CHOSEN'},{'NOS':'NUMBERS'},{'ODD':'ODDS'},{'PAC':'PACKS'},{'PAI':'PAILS'},{'PRS':'PAIRS'},{'PLT':'PALLETS'},{'PCS':'PIECES'},{'LBS':'POUNDS'},{'QTL':'QUINTAL'},{'REL':'REELS'},{'ROL':'ROLLS'},{'SET':'SETS'},{'SHT':'SHEETS'},{'SLB':'SLABS'},{'SQF':'SQUARE FEET'},{'SQI':'SQUARE INCHES'},{'SQC':'SQUARE CENTIMETERS'},{'SQM':'SQUARE METER'},{'SQY':'SQUARE YARDS'},{'BLO':'STEEL BLOCKS'},{'TBL':'TABLES'},{'TBS':'TABLETS'},{'TGM':'TEN GROSS'},{'THD':'THOUSANDS'},{'TIN':'TINS'},{'TOL':'TOLA'},{'TRK':'TRUNK'},{'TUB':'TUBES'},{'UNT':'UNITS'},{'UGS':'US GALLONS'},{'VLS':'VIALS'},{'CSK':'WOODEN CASES'},{'YDS':'YARDS'}]
-                print listofuqc
-                for uom in listofuqc:
+                ''' Following list of uom, first try to insert single uqc if it fail means uqc is exists in table then updated its description and sysunit'''
+                dictofuqc = {'BAG':'BAG','BGS':'BAGS','BLS':'BAILS','BTL':'BOTTLES','BOU':'BOU','BOX':'BOXES','BKL':'BUCKLES','BLK':'BULK','BUN':'BUNCHES','BDL':'BUNDLES','CAN':'CANS','CTN':'CARTONS','CAS':'CASES','CMS':'CENTIMETER','CHI':'CHEST','CLS':'COILS','COL':'COLLIES','CRI':'CRATES','CCM':'CUBIC CENTIMETER','CIN':'CUBIC INCHES','CBM':'CUBIC METER','CQM':'CUBIC METERS','CYL':'CYLINDER','SDM':'DECAMETER SQUARE','DAY':'DAYS','DOZ':'DOZEN','DRM':'DRUMS','FTS':'FEET','FLK':'FLASKS','GMS':'GRAMS','TON':'GREAT BRITAIN TON','GGR':'GREAT GROSS','GRS':'GROSS','GYD':'GROSS YARDS','HBK':'HABBUCK','HKS':'HANKS','HRS':'HOURS','INC':'INCHES','JTA':'JOTTA','KGS':'KILOGRAMS','KLR':'KILOLITER','KME':'KILOMETERS','LTR':'LITERS','LOG':'LOGS','LOT':'LOTS','MTR':'METER','MTS':'METRIC TON','MGS':'MILLIGRAMS','MLT':'MILLILITER','MMT':'MILLIMETER','NONE':'NOT CHOSEN','NOS':'NUMBERS','ODD':'ODDS','PAC':'PACKS','PAI':'PAILS','PRS':'PAIRS','PLT':'PALLETS','PCS':'PIECES','LBS':'POUNDS','QTL':'QUINTAL','REL':'REELS','ROL':'ROLLS','SET':'SETS','SHT':'SHEETS','SLB':'SLABS','SQF':'SQUARE FEET','SQI':'SQUARE INCHES','SQC':'SQUARE CENTIMETERS','SQM':'SQUARE METER','SQY':'SQUARE YARDS','BLO':'STEEL BLOCKS','TBL':'TABLES','TBS':'TABLETS','TGM':'TEN GROSS','THD':'THOUSANDS','TIN':'TINS','TOL':'TOLA','TRK':'TRUNK','TUB':'TUBES','UNT':'UNITS','UGS':'US GALLONS','VLS':'VIALS','CSK':'WOODEN CASES','YDS':'YARDS'}
+                                
+                for unit, desc in dictofuqc.items():
                     try:
-                        uqcname = uom.keys()[0]
-                        uqcdesc = uom.values()[0]
-                        newuqc = self.con.execute(gkdb.unitofmeasurement.insert(),[{"unitname":uqcname,"description":uqcdesc,'conversionrate':0.00,"sysunit":1}])
-                        print uom 
-                        listofuqc.remove(uom)
+                        newuqc = self.con.execute(gkdb.unitofmeasurement.insert(),[{"unitname":unit,"description":desc,'conversionrate':0.00,"sysunit":1}])
                     except:
-                        uom1 = uom.keys()[0]
-                        dataset = {"description":uqcdesc,"sysunit":1}
-                        uomid = self.con.execute(select([gkdb.unitofmeasurement.c.uomid]).where(gkdb.unitofmeasurement.c.unitname == uom1))
+                        uomid = self.con.execute(select([gkdb.unitofmeasurement.c.uomid]).where(gkdb.unitofmeasurement.c.unitname == unit))
                         uqcid = uomid.fetchone()
                         for id in uqcid:
-                            self.con.execute("update unitofmeasurement set sysunit=1, description='%s' where uomid=%d"%(format(uqcdesc),int(id)))
+                            self.con.execute("update unitofmeasurement set sysunit=1, description='%s' where uomid=%d"%(format(desc),int(id)))
+                    dictofuqc.pop(unit,0)
             if not columnExists("organisation","avnoflag"):
                 self.con.execute("alter table organisation add avnoflag integer default 0")
             if not columnExists("organisation","modeflag"):
