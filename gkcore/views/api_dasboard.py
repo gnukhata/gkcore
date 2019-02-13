be491bed4f157c2d8ddff10b3db23e3627d3795c
rupali777 <rupalibdgujar1234@gmail.com>
2019-02-13 11:01:50 +0530

    unpaid five invoice list

--- gkcore/views/api_dasboard.py
+++ gkcore/views/api_dasboard.py
@@ -16,6 +16,7 @@ from operator import itemgetter
 from natsort import natsorted
 @view_defaults(route_name='dashboard')
 class api_dashboard(object):
+    print("inside function")
     """
     This class is a resource for billwise accounting.
 It will be used for creating entries in the billwise table and updating it as new entries are passed.
@@ -26,7 +27,8 @@ It will be used for creating entries in the billwise table and updating it as ne
         self.request = Request
         self.request = request
         self.con = Connection
-    @view_config(request_method='GET',renderer='json', request_param="type=dashboard")
+
+    @view_config(request_method='GET',renderer='json', request_param="type=fiveinvoicelist")
     def getinvoiceatdashboard(self):
 
         try:
@@ -42,21 +44,24 @@ It will be used for creating entries in the billwise table and updating it as ne
 
                 inoutflag = int(self.request.params["inoutflag"])              
                 typeflag = int(self.request.params["typeflag"])
-              
+
                 types = {1:"Amount Wise", 4:"Due Wise"}
-               
+                fiveInvoiceslistdata=[]
                 # Period for which this report is created is determined by startdate and enddate. They are formatted as YYYY-MM-DD.
                 startdate =datetime.strptime(str(self.request.params["startdate"]),"%d-%m-%Y").strftime("%Y-%m-%d")
                 enddate =datetime.strptime(str(self.request.params["enddate"]),"%d-%m-%Y").strftime("%Y-%m-%d")
 
-                # goid is branchid checking branch related invoice when login as branchvise
-                if "goid" in authDetails:
-                    # Invoices in ascending order of amount.
-                    if typeflag == 1:
-                        csInvoices = self.con.execute(select top 5([invoice.c.invid,invoice.c.invoiceno,invoice.c.invoicedate,invoice.c.amountpaid, invoice.c.custid]).where(and_(invoice.c.invoicetotal > invoice.c.amountpaid, invoice.c.icflag == 9, invoice.c.goid == authDetails["goid"],invoice.c.orgcode == authDetails["orgcode"],invoice.c.invoicedate >= startdate, invoice.c.invoicedate <= enddate, invoice.c.inoutflag == inoutflag)).order_by(desc(invoice.c.invoicetotal - invoice.c.amountpaid)))
-                        fiveinvoicelist = fiveInvoices.fetchone()
-                    if typeflag == 4:
-                return{"gkstatus":enumdict["Success"],"invoices":fiveinvoicelist, "type":types[typeflag]}
+                # Invoices in decending order of amount.
+                if typeflag == 1:
+                    fiveinvoices = self.con.execute(select([invoice.c.invid,invoice.c.invoiceno,invoice.c.invoicedate,invoice.c.invoicetotal,invoice.c.amountpaid, invoice.c.custid]).where(and_(invoice.c.invoicetotal > invoice.c.amountpaid, invoice.c.icflag == 9,invoice.c.orgcode == authDetails["orgcode"],invoice.c.invoicedate >= startdate, invoice.c.invoicedate <= enddate, invoice.c.inoutflag == inoutflag)).order_by(desc(invoice.c.invoicetotal - invoice.c.amountpaid)).limit(5))
+                if typeflag == 4:
+                    fiveinvoices = self.con.execute(select([invoice.c.invid,invoice.c.invoiceno,invoice.c.invoicedate,invoice.c.invoicetotal,invoice.c.amountpaid, invoice.c.custid]).where(and_(invoice.c.invoicetotal > invoice.c.amountpaid, invoice.c.icflag == 9,invoice.c.orgcode == authDetails["orgcode"],invoice.c.invoicedate >= startdate, invoice.c.invoicedate <= enddate, invoice.c.inoutflag == inoutflag)).order_by(invoice.c.invoicedate).limit(5))
+                fiveInvoiceslist = fiveinvoices.fetchall()
+                print(fiveInvoiceslist)
+                for inv in fiveInvoiceslist:
+                    fiveInvoiceslistdata.append({"invid":inv["invid"],"invoiceno":inv["invoiceno"],"invoicedate":datetime.strftime(inv["invoicedate"],'%d-%m-%Y'),"balanceamount":"%.2f"%(float(inv["invoicetotal"]-inv["amountpaid"]))})
+
+                return{"gkstatus":enumdict["Success"],"invoices":fiveInvoiceslistdata, "type":types[typeflag]}
                 self.con.close()
             except:
                 return{"gkstatus":enumdict["ConnectionFailed"]}
