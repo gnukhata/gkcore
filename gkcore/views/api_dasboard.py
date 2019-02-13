1d3bcd1ab238f782b147eed7f69ec33eaa52dce7
rupali777 <rupalibdgujar1234@gmail.com>
2019-02-13 12:26:20 +0530

    removd startdate enddate from where condition

--- gkcore/views/api_dasboard.py
+++ gkcore/views/api_dasboard.py
@@ -17,12 +17,7 @@ from natsort import natsorted
 @view_defaults(route_name='dashboard')
 class api_dashboard(object):
     print("inside function")
-    """
-    This class is a resource for billwise accounting.
-It will be used for creating entries in the billwise table and updating it as new entries are passed.
-    The invoice table will also be updated every time an adjustment is made.
-    We will have get and post methods.
-    """
+ 
     def __init__(self, request):
         self.request = Request
         self.request = request
@@ -47,18 +42,17 @@ It will be used for creating entries in the billwise table and updating it as ne
 
                 types = {1:"Amount Wise", 4:"Due Wise"}
                 fiveInvoiceslistdata=[]
-                # Period for which this report is created is determined by startdate and enddate. They are formatted as YYYY-MM-DD.
-                startdate =datetime.strptime(str(self.request.params["startdate"]),"%d-%m-%Y").strftime("%Y-%m-%d")
-                enddate =datetime.strptime(str(self.request.params["enddate"]),"%d-%m-%Y").strftime("%Y-%m-%d")
-
-                # Invoices in decending order of amount.
+               
+                # Invoices in descending order of amount.
                 if typeflag == 1:
-                    fiveinvoices = self.con.execute(select([invoice.c.invid,invoice.c.invoiceno,invoice.c.invoicedate,invoice.c.invoicetotal,invoice.c.amountpaid, invoice.c.custid]).where(and_(invoice.c.invoicetotal > invoice.c.amountpaid, invoice.c.icflag == 9,invoice.c.orgcode == authDetails["orgcode"],invoice.c.invoicedate >= startdate, invoice.c.invoicedate <= enddate, invoice.c.inoutflag == inoutflag)).order_by(desc(invoice.c.invoicetotal - invoice.c.amountpaid)).limit(5))
+                    fiveinvoices = self.con.execute(select([invoice.c.invid,invoice.c.invoiceno,invoice.c.invoicedate,invoice.c.invoicetotal,invoice.c.amountpaid, invoice.c.custid]).where(and_(invoice.c.invoicetotal > invoice.c.amountpaid, invoice.c.icflag == 9,invoice.c.orgcode == authDetails["orgcode"], invoice.c.inoutflag == inoutflag)).order_by(desc(invoice.c.invoicetotal - invoice.c.amountpaid)).limit(5))
+                # Invoices in ascending order of date.                
                 if typeflag == 4:
-                    fiveinvoices = self.con.execute(select([invoice.c.invid,invoice.c.invoiceno,invoice.c.invoicedate,invoice.c.invoicetotal,invoice.c.amountpaid, invoice.c.custid]).where(and_(invoice.c.invoicetotal > invoice.c.amountpaid, invoice.c.icflag == 9,invoice.c.orgcode == authDetails["orgcode"],invoice.c.invoicedate >= startdate, invoice.c.invoicedate <= enddate, invoice.c.inoutflag == inoutflag)).order_by(invoice.c.invoicedate).limit(5))
+                    fiveinvoices = self.con.execute(select([invoice.c.invid,invoice.c.invoiceno,invoice.c.invoicedate,invoice.c.invoicetotal,invoice.c.amountpaid, invoice.c.custid]).where(and_(invoice.c.invoicetotal > invoice.c.amountpaid, invoice.c.icflag == 9,invoice.c.orgcode == authDetails["orgcode"], invoice.c.inoutflag == inoutflag)).order_by(invoice.c.invoicedate).limit(5))
                 fiveInvoiceslist = fiveinvoices.fetchall()
 
                 for inv in fiveInvoiceslist:
+                    # for fetch customer or supplier name using cust id in invoice.
                     csd = self.con.execute(select([customerandsupplier.c.custname, customerandsupplier.c.csflag]).where(and_(customerandsupplier.c.custid == inv["custid"],customerandsupplier.c.orgcode==authDetails["orgcode"])))
                     csDetails = csd.fetchone()
                     fiveInvoiceslistdata.append({"invid":inv["invid"],"invoiceno":inv["invoiceno"],"invoicedate":datetime.strftime(inv["invoicedate"],'%d-%m-%Y'),"balanceamount":"%.2f"%(float(inv["invoicetotal"]-inv["amountpaid"])), "custname":csDetails["custname"],"csflag":csDetails["csflag"]})
