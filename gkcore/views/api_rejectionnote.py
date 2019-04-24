ee4d1db8ef670abc8f6e7e0bcb983db1a933f028
abhi1tb <abhijithb21@disroot.org>
2019-04-24 11:37:24 +0530

    New condition in where clause for query fetching rnid ino rejection note post

--- gkcore/views/api_rejectionnote.py
+++ gkcore/views/api_rejectionnote.py
@@ -74,7 +74,7 @@ class api_rejectionnote(object):
                 items = rejectionnotedata["rejprods"]
                 result = self.con.execute(rejectionnote.insert(),[rejectionnotedata])
                 if result.rowcount==1:
-                    rniddata = self.con.execute(select([rejectionnote.c.rnid,rejectionnote.c.rndate]).where(and_(rejectionnote.c.orgcode==authDetails["orgcode"],rejectionnote.c.rnno==rejectionnotedata["rnno"])))
+                    rniddata = self.con.execute(select([rejectionnote.c.rnid,rejectionnote.c.rndate]).where(and_(rejectionnote.c.orgcode==authDetails["orgcode"],rejectionnote.c.rnno==rejectionnotedata["rnno"], rejectionnote.c.inout==rejectionnotedata["inout"])))
                     rnidrow = rniddata.fetchone()
                     stockdata["dcinvtnid"] = rnidrow["rnid"]
                     stockdata["dcinvtnflag"] = 18
