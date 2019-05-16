5e839b646a4186094bd0ce8a4cae47f79cf29cff
rupali777 <rupalibdgujar1234@gmail.com>
2019-05-16 12:06:04 +0530

    changes

--- gkcore/views/api_dasboard.py
+++ gkcore/views/api_dasboard.py
@@ -272,10 +272,10 @@ class api_dashboard(object):
                     delchal_out=delchalcountbymonth(9,orgcode)
                     delchal_in=delchalcountbymonth(15,orgcode)
                     return{"gkstatus":enumdict["Success"],"userrole":userrole,"gkresult":{"puchaseinvcount":purchase_inv["invamount"],"saleinvcount":sale_inv["invamount"],"delchalout":delchal_out["totalamount"],"delchalin":delchal_in["totalamount"]}}
-                # if userrole == 3:
-                #     delchal_out=delchalcountbymonth(9,orgcode)
-                #     delchal_in=delchalcountbymonth(15,orgcode)
-                #     return{"gkstatus":enumdict["Success"],"userrole":userrole,"gkresult":{"delchalout":delchal_out["totalamount"],"delchalin":delchal_in["totalamount"]}}
+                if userrole == 3:
+                    delchal_out=delchalcountbymonth(9,orgcode)
+                    delchal_in=delchalcountbymonth(15,orgcode)
+                    return{"gkstatus":enumdict["Success"],"userrole":userrole,"gkresult":{"delchalout":delchal_out["totalamount"],"delchalin":delchal_in["totalamount"]}}
             except:
                 return{"gkstatus":enumdict["ConnectionFailed"]}
 
