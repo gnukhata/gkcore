a6418ffff4510f7d9c2481a34d4cdc53d178c86f
rohan <rohankhairnar@gmail.com>
2019-08-13 16:54:46 +0530

    purchaseorder on prior

--- gkcore/views/api_organisation.py
+++ gkcore/views/api_organisation.py
@@ -120,13 +120,13 @@ class api_organisation(object):
                 self.con.execute("alter table delchal add column roundoffflag integer default 0")
                 self.con.execute("alter table drcr add column roundoffflag integer default 0")
             # remove goid if present
-            if columnExists("rejectionnote","goid"):
+            if columnExists("purchaseorder","goid"):
+                self.con.execute("alter table purchaseorder drop column goid")
                 self.con.execute("alter table rejectionnote drop column goid")
                 self.con.execute("alter table drcr drop column goid")
                 self.con.execute("alter table budget drop column goid")
                 self.con.execute("alter table vouchers drop column goid")
                 self.con.execute("alter table invoice drop column goid")
-                self.con.execute("alter table purchaseorder drop column goid")
                 self.con.execute("alter table delchal drop column goid")
                 
 
