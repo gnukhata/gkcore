ec1323bbfc6f5360848e71af1ba3a8a6e322e498
rupali777 <rupalibdgujar1234@gmail.com>
2019-09-26 13:14:56 +0530

    revert cancelflag changes at delchal

--- gkcore/views/api_organisation.py
+++ gkcore/views/api_organisation.py
@@ -147,7 +147,7 @@ class api_organisation(object):
                         rorAdd = self.con.execute(gkdb.accounts.insert(),[{"accountname":"Round Off Received","groupcode":grpCodeR["groupcode"],"orgcode":orgcode["orgcode"],"defaultflag":181}])
 
             #In Below query we are adding field pincode to invoice table
-            if not columnExists("invoice","pincode"):
+            if not columnExists("invoice","pincode"):   
                 self.con.execute("alter table invoice add pincode text")
             #In Below query we are adding field pincode to invoicebin table
             if not columnExists("invoicebin","pincode"):
@@ -158,7 +158,6 @@ class api_organisation(object):
             #Below query is to remove gbflag if it exists.
             if columnExists("godown","gbflag"):
                 self.con.execute("alter table godown drop column gbflag")
-
             #In Below query we are adding field pincode to purchaseorder table
             if not columnExists("purchaseorder","pincode"):
                 self.con.execute("alter table purchaseorder add pincode text")
