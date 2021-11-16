ddf2c1c271b453a6c85a65ffb7bfacb63a47853d
Sai Karthik <kskarthik@disroot.org>
2021-11-16 22:40:35 +0530

    refactor(ifsc): added auth check, align status code with rest of api's

--- gkcore/views/api_ifsc.py
+++ gkcore/views/api_ifsc.py
@@ -1,20 +1,46 @@
 from pyramid.request import Request
+from gkcore import enumdict
 from pyramid.view import view_defaults, view_config
-import requests
+from gkcore.views.api_login import authCheck
+import requests, os
 
 @view_defaults(route_name="ifsc")
-class validate_ifsc(object):
+class IFSC(object):
     def __init__(self, request):
         self.request = Request
         self.request = request
 
     @view_config(request_method="GET", request_param="check", renderer="json")
-    def validateIFSC(self):
-        server = "http://localhost:3000"
+    def validate_ifsc(self):
+        """
+        Validate provided IFSC code
+
+        This method first checks the user auth status, Then validates given
+        ifsc code with the razorpay/ifsc docker server & get's the response
+        """
+        try:
+            token = self.request.headers["gktoken"]
+        except:
+            return {"gkstatus": enumdict["UnauthorisedAccess"]}
+
+        auth_details = authCheck(token)
+
+        if auth_details["auth"] == False:
+            return {"gkstatus": enumdict["UnauthorisedAccess"]}
+
+        ifsc_server = "http://localhost:6545"
+
+        # check for custom IFSC server URL & set it when provided
+        custom_ifsc_server = os.getenv('GKCORE_IFSC_SERVER')
+
+        if custom_ifsc_server != None:
+            ifsc_server = custom_ifsc_server
+
         ifsc_code = self.request.params["check"]
-        api_response = requests.get(f"{server}/{ifsc_code}")
-        result = {"is_valid": "false"}
+        api_response = requests.get(f"{ifsc_server}/{ifsc_code}")
+        result = {"gkstatus": enumdict['ConnectionFailed']}
+
         if api_response.status_code == 200:
-            result["is_valid"] = "true"
+            result["gkstatus"] = enumdict['Success']
             result["gkresult"] = api_response.json()
         return result
