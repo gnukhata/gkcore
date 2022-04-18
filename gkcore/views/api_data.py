33edaec884ea905fd6adc32f886aac209dd81167
Sai Karthik <kskarthik@disroot.org>
2022-04-19 01:01:24 +0530

    feat: mv json import/export code to data module

--- gkcore/views/api_data.py
+++ gkcore/views/api_data.py
@@ -35,11 +35,10 @@ from openpyxl import Workbook
 from openpyxl.styles import Font
 import json, io
 from gkcore.models.meta import gk_api, dbconnect
-
 from sqlalchemy.engine.base import Connection
 from gkcore import eng, enumdict
 from gkcore.views.api_user import authCheck, getUserRole
-import xml.etree.ElementTree as ET
+import gkcore.views.data as data
 
 # import datetime
 
@@ -339,58 +338,8 @@ class api_data(object):
             return {"gkstatus": 3}
 
     @view_config(request_param="import-json", request_method="POST", renderer="json")
-    def import_json(self):
-        """Import org data from GNUKhata's json format"""
-        try:
-            header = {"gktoken": self.request.headers["gktoken"]}
-            f = self.request.POST["gkfile"].file
-            org = json.load(f)
-
-            # customers / suppliers
-            print("\n 🤝 importing customers/suppliers ...")
-            for i in org["customerandsupplier"]:
-                # remove foreign key
-                i.pop("custid")
-
-                response = gk_api(
-                    method="POST",
-                    url="/customersupplier",
-                    body=i,
-                    header=header,
-                    request=self.request,
-                )
-                print(i["custname"], response)
-            # Godowns
-            print("\n 📦 Importing Godowns ...")
-            for i in org["godown"]:
-
-                i.pop("goid")
-                response = gk_api(
-                    method="POST",
-                    url="/godown",
-                    body=i,
-                    header=header,
-                    request=self.request,
-                )
-                print(i["goname"], response)
-
-            # Invoice
-            print("\n 📦 Importing Invoicebin ...")
-            for i in org["invoicebin"]:
-                i.pop("goid")
-                response = gk_api(
-                    method="POST",
-                    url="/godown",
-                    body=i,
-                    header=header,
-                    request=self.request,
-                )
-                print(i["goname"], response)
-
-            return {"gkstatus": 0}
-        except Exception as e:
-            print(e)
-            return {"gkstatus": 3}
+    def ij(self):
+        return data.json_handler.import_json(self)
 
     @view_config(request_param="export", request_method="GET", renderer="json")
     def export_ledger(self):
@@ -537,107 +486,5 @@ class api_data(object):
             return {"gkstatus": 3}
 
     @view_config(request_param="export-json", request_method="GET", renderer="json")
-    def export_json(self):
-        """Export all database tables to a json file"""
-
-        org_code = None
-
-        # Check & validate user access
-        try:
-            token = self.request.headers["gktoken"]
-            user = authCheck(token)
-            print(user)
-            org_code = user["orgcode"]
-            user_role = getUserRole(user["userid"])["gkresult"]["userrole"]
-
-            # only admin can export data
-            print(user_role)
-            if user_role != -1:
-                return {"gkstatus": enumdict["BadPrivilege"]}
-        except:
-            return {"gkstatus": enumdict["UnauthorisedAccess"]}
-
-        db_tables = dbconnect().table_names()
-
-        data = {}
-
-        ignored_tables = [
-            "log",
-            "state",
-            "users",
-            "organisation",
-            "signature",
-            "unitofmeasurement",
-        ]
-
-        for n in db_tables:
-            if n not in ignored_tables:
-                data[n] = get_table_array(name=n, orgcode=org_code)
-
-        file_obj = io.StringIO()
-        json.dump(data, file_obj, default=type_cast, indent=1)
-        export_file = file_obj.getvalue()
-        file_obj.close()
-        headerList = {
-            "Content-Type": "application/json; charset=utf-8",
-            "Content-Length": len(data),
-            "Content-Disposition": "attachment; filename=report.json",
-            "X-Content-Type-Options": "nosniff",
-            "Set-Cookie": "fileDownload=true; path=/ ;HttpOnly",
-        }
-
-        return Response(
-            export_file,
-            headerlist=list(headerList.items()),
-        )
-
-    @view_config(
-        request_param="import-tally-xml", request_method="POST", renderer="json"
-    )
-    def import_tally_xml(self):
-        with open("/home/kar/Downloads/Master.xml", "r", encoding="utf-8") as f:
-            file = ET.parse(f)
-            print(file)
-            return 0
-
-
-def get_table_array(name: str, orgcode: int):
-    """Return given sql table contents as an array of dicts
-
-    *params*
-
-    `name`: db table name
-
-    `orgcode`: integer
-
-    """
-    c = eng.connect()
-    table = c.execute(f"select * from {name} where orgcode = {orgcode}")
-    a = []
-    for row in table:
-        d = {}
-        for i in row.keys():
-            d[i] = row[i]
-        # delete orgcode key as it's not required during import
-        d.pop("orgcode", None)
-        a.append(d)
-    return a
-
-
-def type_cast(key):
-    """Convert sql data types to json compatable one's"""
-
-    key_type = str(type(key))
-
-    if key_type == "<class 'decimal.Decimal'>":
-        return float(key)
-
-    if key_type == "<class 'datetime.datetime'>":
-        return str(key)
-
-    if key_type == "<class 'datetime.date'>":
-        return str(key)
-
-    else:
-        print(key_type)
-        return str(key)
+    def ej(self):
+        return data.json_handler.export_json(self)
