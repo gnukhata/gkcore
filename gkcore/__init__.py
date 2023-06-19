6edc711a856b9949cc2dde87908c6b26ed4d9af5
Sai Karthik <kskarthik@disroot.org>
2023-06-19 20:23:49 +0530

    fix(openapi): /spec route

--- gkcore/__init__.py
+++ gkcore/__init__.py
@@ -220,7 +220,7 @@ def main(global_config, **settings):
     config.include("pyramid_openapi3")
     config.add_static_view(name="spec", path="spec")
     config.pyramid_openapi3_spec_directory(
-        os.path.join(os.path.dirname(__file__), "spec/main.yaml")
+        os.path.join(os.path.dirname(__file__), "spec/main.yaml"), route="/spec"
     )
     # launch the swagger ui at /docs/
     config.pyramid_openapi3_add_explorer(route="/docs/", ui_version="5.1.0")
