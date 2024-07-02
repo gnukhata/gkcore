29f3b5dff1980b53400901e23b5ca9ad6e8a0fe7
Kannan V M <kannan@disroot.org>
2024-07-02 23:45:20 +0530

    refactor: update Unit of Measurement API
    In this commit `api_unitOfMeasurement` is updated to,
    1. use security policy
    2. use context managers for DBAPI connection
    3. use seperate to function for query instead of seperate API call

--- gkcore/views/api_unitofmeasurement.py
+++ gkcore/views/api_unitofmeasurement.py
@@ -23,63 +23,53 @@ Contributors:
 "Krishnakant Mane" <kk@gmail.com>
 "Ishan Masdekar " <imasdekar@dff.org.in>
 "Navin Karkera" <navin@dff.org.in>
+"Kannan V M" <@kannan:poddery.com>
 """
 
-
-from requests import request
 from gkcore import eng, enumdict
 from gkcore.models import gkdb
 from sqlalchemy.sql import select
-from sqlalchemy.engine.base import Connection
-from sqlalchemy import and_, exc, desc, func
-from pyramid.request import Request
+from sqlalchemy import func
 from pyramid.view import view_defaults, view_config
 import gkcore
-from gkcore.models.meta import gk_api
-from gkcore.utils import authCheck, gk_log
 
 
-@view_defaults(route_name="uom")
+def is_uom_used(connection, uomid):
+    countresult = connection.execute(
+        select([func.count(gkdb.product.c.uomid).label("subcount")]).where(
+            gkdb.product.c.uomid == uomid
+        )
+    )
+    countrow = countresult.fetchone()
+    subcount = countrow["subcount"]
+    if subcount > 0:
+        return True
+    return False
+
+
+@view_defaults(route_name="uom", permission="read")
 class api_unitOfMeasurement(object):
     def __init__(self, request):
-        self.request = Request
         self.request = request
-        self.con = Connection
 
-    @view_config(request_method="POST", renderer="json")
+    @view_config(request_method="POST", renderer="json", permission="write")
     def addUnitOfMeasurement(self):
-        try:
-            token = self.request.headers["gktoken"]
-        except:
-            return {"gkstatus": gkcore.enumdict["UnauthorisedAccess"]}
-        authDetails = authCheck(token)
-        if authDetails["auth"] == False:
-            return {"gkstatus": enumdict["UnauthorisedAccess"]}
-        else:
-            try:
-                self.con = eng.connect()
-                dataset = self.request.json_body
-                # Check for duplicate entry before insertion
-                result_duplicate_check = self.con.execute(
-                    select([gkdb.unitofmeasurement.c.unitname]).where(
-                        and_(
-                            func.lower(gkdb.unitofmeasurement.c.unitname) == func.lower(dataset["unitname"]),
-                        )
-                    )
-                )
-                
-                if result_duplicate_check.rowcount > 0:
-                    # Duplicate entry found, handle accordingly
-                    return {"gkstatus": enumdict["DuplicateEntry"]}
 
-                result = self.con.execute(gkdb.unitofmeasurement.insert(), [dataset])
-                return {"gkstatus": enumdict["Success"]}
-            except exc.IntegrityError:
+        with eng.begin() as con:
+            dataset = self.request.json_body
+            # Check for duplicate entry before insertion
+            result_duplicate_check = con.execute(
+                select([gkdb.unitofmeasurement.c.unitname]).where(
+                    func.lower(gkdb.unitofmeasurement.c.unitname)
+                    == func.lower(dataset["unitname"]),
+                )
+            )
+            if result_duplicate_check.rowcount > 0:
+                # Duplicate entry found, handle accordingly
                 return {"gkstatus": enumdict["DuplicateEntry"]}
-            except:
-                return {"gkstatus": gkcore.enumdict["ConnectionFailed"]}
-            finally:
-                self.con.close()
+
+            con.execute(gkdb.unitofmeasurement.insert(), [dataset])
+            return {"gkstatus": enumdict["Success"]}
 
     """
     This function is for the condition that the units of measurement which
@@ -88,157 +78,88 @@ class api_unitOfMeasurement(object):
 
     @view_config(route_name="uom-single", request_method="GET", renderer="json")
     def getUnitOfMeasurement(self):
-        try:
-            token = self.request.headers["gktoken"]
-        except:
-            return {"gkstatus": enumdict["UnauthorisedAccess"]}
-        authDetails = authCheck(token)
-        if authDetails["auth"] == False:
-            return {"gkstatus": enumdict["UnauthorisedAccess"]}
-        else:
-            try:
-                self.con = eng.connect()
-                dataset = self.request.matchdict
-                result = self.con.execute(
-                    select([gkdb.unitofmeasurement]).where(
-                        gkdb.unitofmeasurement.c.uomid == dataset["uomid"]
-                    )
+        with eng.connect() as con:
+            dataset = self.request.matchdict
+            result = con.execute(
+                select([gkdb.unitofmeasurement]).where(
+                    gkdb.unitofmeasurement.c.uomid == dataset["uomid"]
                 )
-                row = result.fetchone()
-                unitofmeasurement = {
-                    "uomid": row["uomid"],
-                    "unitname": row["unitname"],
-                    "conversionrate": "%.2f" % float(row["conversionrate"]),
-                    "subunitof": row["subunitof"],
-                    "description": row["description"],
-                    "sysunit": row["sysunit"],
-                    "uqc": row["uqc"],
-                }
-                countresult = self.con.execute(
-                    select([func.count(gkdb.product.c.uomid).label("subcount")]).where(
-                        gkdb.product.c.uomid == row["uomid"]
-                    )
-                )
-                countrow = countresult.fetchone()
-                subcount = countrow["subcount"]
-                if subcount > 0:
-                    unitofmeasurement["flag"] = "True"
-                else:
-                    unitofmeasurement["flag"] = "False"
-                return {
-                    "gkstatus": gkcore.enumdict["Success"],
-                    "gkresult": unitofmeasurement,
-                }
-            except:
-                return {"gkstatus": gkcore.enumdict["ConnectionFailed"]}
-            finally:
-                self.con.close()
+            )
+            row = result.fetchone()
+            unitofmeasurement = {
+                "uomid": row["uomid"],
+                "unitname": row["unitname"],
+                "conversionrate": "%.2f" % float(row["conversionrate"]),
+                "subunitof": row["subunitof"],
+                "description": row["description"],
+                "sysunit": row["sysunit"],
+                "uqc": row["uqc"],
+                "flag": is_uom_used(con, row["uomid"]),
+            }
 
-    @view_config(request_method="PUT", renderer="json")
+            return {
+                "gkstatus": gkcore.enumdict["Success"],
+                "gkresult": unitofmeasurement,
+            }
+
+    @view_config(request_method="PUT", renderer="json", permission="update")
     def editunitofmeasurement(self):
-        try:
-            token = self.request.headers["gktoken"]
-        except:
-            return {"gkstatus": gkcore.enumdict["UnauthorisedAccess"]}
-        authDetails = authCheck(token)
-        if authDetails["auth"] == False:
-            return {"gkstatus": enumdict["UnauthorisedAccess"]}
-        else:
-            try:
-                self.con = eng.connect()
-                dataset = self.request.json_body
-                result = self.con.execute(
-                    gkdb.unitofmeasurement.update()
-                    .where(gkdb.unitofmeasurement.c.uomid == dataset["uomid"])
-                    .values(dataset)
-                )
-                return {"gkstatus": enumdict["Success"]}
-            except:
-                return {"gkstatus": gkcore.enumdict["ConnectionFailed"]}
-            finally:
-                self.con.close()
+        with eng.begin() as con:
+            con = eng.connect()
+            dataset = self.request.json_body
+            con.execute(
+                gkdb.unitofmeasurement.update()
+                .where(gkdb.unitofmeasurement.c.uomid == dataset["uomid"])
+                .values(dataset)
+            )
+            return {"gkstatus": enumdict["Success"]}
 
-    @view_config(request_method="GET", renderer="json")
+    @view_config(request_method="GET", renderer="json", is_authenticated=True)
     def getAllunitofmeasurements(self):
-        try:
-            token = self.request.headers["gktoken"]
-        except:
-            return {"gkstatus": gkcore.enumdict["UnauthorisedAccess"]}
-        authDetails = authCheck(token)
-        if authDetails["auth"] == False:
-            return {"gkstatus": gkcore.enumdict["UnauthorisedAccess"]}
-        else:
-            try:
-                self.con = eng.connect()
-                # there is only one possibility for a catch which is failed connection to db.
-                result = self.con.execute(
-                    select(
-                        [
-                            gkdb.unitofmeasurement.c.unitname,
-                            gkdb.unitofmeasurement.c.uomid,
-                            gkdb.unitofmeasurement.c.description,
-                            gkdb.unitofmeasurement.c.subunitof,
-                            gkdb.unitofmeasurement.c.sysunit,
-                            gkdb.unitofmeasurement.c.uqc,
-                        ]
-                    ).order_by(gkdb.unitofmeasurement.c.unitname)
+        with eng.connect() as con:
+            con = eng.connect()
+            result = con.execute(
+                select(
+                    [
+                        gkdb.unitofmeasurement.c.unitname,
+                        gkdb.unitofmeasurement.c.uomid,
+                        gkdb.unitofmeasurement.c.description,
+                        gkdb.unitofmeasurement.c.subunitof,
+                        gkdb.unitofmeasurement.c.sysunit,
+                        gkdb.unitofmeasurement.c.uqc,
+                    ]
+                ).order_by(gkdb.unitofmeasurement.c.unitname)
+            )
+            unitofmeasurements = []
+            for row in result:
+                unitofmeasurements.append(
+                    {
+                        "uomid": row["uomid"],
+                        "unitname": row["unitname"],
+                        "description": row["description"],
+                        "subunitof": row["subunitof"],
+                        "sysunit": row["sysunit"],
+                        "uqc": row["uqc"],
+                    }
                 )
-                unitofmeasurements = []
-                for row in result:
-                    unitofmeasurements.append(
-                        {
-                            "uomid": row["uomid"],
-                            "unitname": row["unitname"],
-                            "description": row["description"],
-                            "subunitof": row["subunitof"],
-                            "sysunit": row["sysunit"],
-                            "uqc": row["uqc"],
-                        }
-                    )
-                return {
-                    "gkstatus": gkcore.enumdict["Success"],
-                    "gkresult": unitofmeasurements,
-                }
-            except:
-                return {"gkstatus": gkcore.enumdict["ConnectionFailed"]}
-            finally:
-                self.con.close()
+            return {
+                "gkstatus": gkcore.enumdict["Success"],
+                "gkresult": unitofmeasurements,
+            }
 
-    @view_config(request_method="DELETE", renderer="json")
+    @view_config(request_method="DELETE", renderer="json", permission="delete")
     def deleteunitofmeasurement(self):
         """Delete a Unit of measurement. Do not delete UOM which is associated with a product"""
-        try:
-            token = self.request.headers["gktoken"]
-        except:
-            return {"gkstatus": enumdict["UnauthorisedAccess"]}
-        authDetails = authCheck(token)
-        if authDetails["auth"] == False:
-            return {"gkstatus": enumdict["UnauthorisedAccess"]}
-        else:
-            try:
-                self.con = eng.connect()
-                dataset = self.request.json_body
-                # check if uom is associated with any product/service
-                check_uom = gk_api(
-                    url=f"/unitofmeasurement/{dataset['uomid']}",
-                    request=self.request,
-                    header=self.request.headers,
-                )
-                gk_log(__name__).warn(check_uom)
-                # if it is used in a product/service, do not delete it
-                if check_uom["gkresult"]["flag"] == "True":
-                    return {"gkstatus": enumdict["ActionDisallowed"]}
-                # proceed to deletion if not used
-                self.con.execute(
-                    gkdb.unitofmeasurement.delete().where(
-                        gkdb.unitofmeasurement.c.uomid == dataset["uomid"]
-                    )
-                )
-                return {"gkstatus": enumdict["Success"]}
-            except exc.IntegrityError:
+        with eng.begin() as con:
+            dataset = self.request.json_body
+            # Check if uom is associated with any product/service.
+            # If it is used in a product/service, do not delete it.
+            if is_uom_used(con, dataset["uomid"]):
                 return {"gkstatus": enumdict["ActionDisallowed"]}
-            except Exception as e:
-                gk_log(__name__).error(e)
-                return {"gkstatus": enumdict["ConnectionFailed"]}
-            finally:
-                self.con.close()
+            # proceed to deletion if not used
+            con.execute(
+                gkdb.unitofmeasurement.delete().where(
+                    gkdb.unitofmeasurement.c.uomid == dataset["uomid"]
+                )
+            )
+            return {"gkstatus": enumdict["Success"]}
