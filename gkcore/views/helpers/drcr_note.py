6bc8cbbd29017998ab34643cc2813eed74decbbc
Kannan V M <kannan@disroot.org>
2024-08-01 18:34:51 +0530

    fix: update Dr/Cr note to correctly calculate sign of entries and totals

--- gkcore/views/helpers/drcr_note.py
+++ gkcore/views/helpers/drcr_note.py
@@ -29,25 +29,34 @@ def get_drcr_note_details(connection, drcr_id):
         tax_name = "VAT"
     elif invoice_details["taxflag"] == 7:
         if invoice_details["sourcestate"] == invoice_details["sourcestate"]:
-            tax_name = "SGST"
+            tax_name = "SGST" # "SGST" string handles both "SGST" and "CGST" for now
         else:
             tax_name = "IGST"
 
     total_amount = 0
     for item_id, qty in drcr_note_qty.items():
-        tax_percent = invoice_details["tax"][item_id]
-        price_taxfree = float(qty)*float(drcr_note_price[item_id])
-        tax_percent = float(tax_percent)/2 if tax_name == "SGST" else float(tax_percent)
-        drcr_sign = -1 if drcr_note_details["dctypeflag"] == 3 else 1
-        tax_amount = drcr_sign*price_taxfree*tax_percent/100
-        total_amount += (
-            drcr_sign*price_taxfree+tax_amount*(2 if tax_name == "SGST" else 1)
-        )
-        if tax_amount == 0:
+        tax_percent = float(invoice_details["tax"][item_id])
+        if tax_percent == 0:
             continue
+        price_taxfree = float(qty)*float(drcr_note_price[item_id])
+        tax_amount = price_taxfree*tax_percent/100
+        taxed_amount = price_taxfree+tax_amount
+        # sale dr note: tax liability increases, sale increases
+        # sale cr note: tax liability decreases, sale decreases
+        # purchase dr note: tax asset increases, purchase increases
+        # purchase cr note: tax asset decreases, purchase decreases
+        #
+        # for both sale and purchase,
+        # debit note: increases the value (sales, purchase, tax)
+        # credit note: decreases the value (sales, purchase, tax)
+        drcr_note_sign = -1 if drcr_note_details["dctypeflag"] == 3 else 1
+        total_amount += drcr_note_sign*taxed_amount
+        if tax_name == "SGST": # For SGST and CGST tax amounts are half of the total GST
+            tax_percent = tax_percent/2
+            tax_amount = tax_amount/2
         item_tax_details = {
             "product_id": item_id,
-            "tax_amount": f"{tax_amount:.2f}",
+            "tax_amount": drcr_note_sign*tax_amount,
             "tax_name": tax_name,
             "tax_str": f"{tax_percent}% {tax_name}",
             "tax_percent": tax_percent,
@@ -58,7 +67,7 @@ def get_drcr_note_details(connection, drcr_id):
             drcr_tax_details.append(
                 {
                     "product_id": item_id,
-                    "tax_amount": f"{tax_amount:.2f}",
+                    "tax_amount": drcr_note_sign*tax_amount,
                     "tax_name": tax_name,
                     "tax_str": f"{tax_percent}% {tax_name}",
                     "tax_percent": tax_percent,
