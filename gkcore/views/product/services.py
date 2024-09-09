64cfae74269c31e77b895c8c5bc1a0659246cef4
Kannan V M <kannan@disroot.org>
2024-09-10 00:23:00 +0530

    feat: add account name validation for item creation/updation APIs

--- gkcore/views/product/services.py
+++ gkcore/views/product/services.py
@@ -1,6 +1,6 @@
-from sqlalchemy import and_, select
+from sqlalchemy import and_, or_, select
 from gkcore import eng
-from gkcore.models.gkdb import product
+from gkcore.models.gkdb import product, accounts
 
 
 def check_duplicate_item_name(item_name, orgcode, current_item_code=None):
@@ -22,3 +22,31 @@ def check_duplicate_item_name(item_name, orgcode, current_item_code=None):
         product_results = conn.execute(statement)
         if product_results.rowcount:
             raise ValueError("This name is already used.")
+
+
+def check_duplicate_item_account_name(account_name, orgcode, current_productcode=None):
+    """Check for account entries with same name. Requires `account_name` and `orgcode`.
+    `current_productcode` is required to exclude the current item from the filter while
+    item name is being updated.
+    """
+    with eng.connect() as conn:
+        if current_productcode:
+            item = conn.execute(
+                select([product.c.productdesc]).where(
+                    product.c.productcode == current_productcode,
+                )
+            ).fetchone()
+            if item["productdesc"] == account_name:
+                return
+        statement = select([accounts]).where(
+            and_(
+                or_(
+                    accounts.c.accountname == account_name+" Sale",
+                    accounts.c.accountname == account_name+" Purchase",
+                ),
+                accounts.c.orgcode == orgcode,
+            )
+        )
+        item_results = conn.execute(statement)
+        if item_results.rowcount:
+            raise ValueError("This name is not available.")
