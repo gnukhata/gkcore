aba8163d10950a3a0e6e39565329be4e22f97e21
Bady <bady@disroot.org>
2024-10-28 10:00:32 +0530

    fix: fetching godown details for services throws error in getDrCrDetails method

--- gkcore/views/api_drcrnote.py
+++ gkcore/views/api_drcrnote.py
@@ -447,6 +447,7 @@ class api_drcr(object):
                             )
 
                         goidrow = goid_result.fetchall()
+                        goid = goidrow[0][0] if goidrow else None
                         if invrow["sourcestate"] != invrow["taxstate"]:
                             taxname = "IGST"
                             taxAmount = reductprice * (taxRate / 100)
@@ -474,7 +475,7 @@ class api_drcr(object):
                             "newtaxableamnt": "%.2f" % (float(reductprice)),
                             "reductionval": "%.2f" % float(idrateData[pc]),
                             "gsflag": prodrow["gsflag"],
-                            "goid": goidrow[0][0],
+                            "goid": goid,
                         }
             drcrdata["totaltaxablevalue"] = "%.2f" % (float(totalTaxableVal))
             drcrdata["totaltaxamt"] = "%.2f" % (float(totalTaxAmt))
