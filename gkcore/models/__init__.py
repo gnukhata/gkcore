86a3b79092ca88d37a0601db30d4133e2dff4d67
Bady <bady@disroot.org>
2024-11-28 11:34:55 +0530

    fix: handle fallback from empty strings to default database host and db values

--- gkcore/models/__init__.py
+++ gkcore/models/__init__.py
@@ -63,8 +63,8 @@ def generate_db_url():
                 f"postgresql+psycopg2://{db_user}:{db_password}@/{db_name}?host={unix_sock_path}"
             )
         else:
-            db_host = os.environ.get("GKCORE_DB_HOST", "localhost")
-            db_port = os.environ.get("GKCORE_DB_PORT", 5432)
+            db_host = os.environ.get("GKCORE_DB_HOST") or "localhost"
+            db_port = os.environ.get("GKCORE_DB_PORT") or 5432
             db_url = (
                 f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
             )
