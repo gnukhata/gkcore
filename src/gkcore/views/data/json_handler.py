127233e0a8b2c37132c388e7af018358bf66d8fb
Kannan V M <kannan@disroot.org>
2024-12-06 14:34:43 +0530

    fix: fix update_json_fields errors
    This will fix,
    - Type check for item (currently it supports only int)
    - Type change to string before updating the entry, the stored entry
    is in string format

--- src/gkcore/views/data/json_handler.py
+++ src/gkcore/views/data/json_handler.py
@@ -290,7 +290,9 @@ def update_json_fields(con: Connection, table: Table, pk_map: dict) -> None:
             if key_related_json_fields and (field_name in key_related_json_fields):
                 related_table_name = key_related_json_fields[field_name]
                 for item in value.keys():
-                    if not type(item) == int:
+                    try:
+                        item = int(item)
+                    except ValueError:
                         continue
                     related_value = pk_map[related_table_name][int(item)]
                     con.execute(
@@ -300,8 +302,8 @@ def update_json_fields(con: Connection, table: Table, pk_map: dict) -> None:
                         .values(
                             {
                                 field_name: func.jsonb_set(
-                                    field, '{'+str(related_value)+'}', field[item]
-                                ).op('-')(item)
+                                    field, '{'+str(related_value)+'}', field[str(item)]
+                                ).op('-')(str(item))
                             }
                         )
                     )
