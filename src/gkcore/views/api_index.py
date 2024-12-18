52ca459777f1404c8ecebe4586fa83ed8c646196
Kannan V M <kannan@disroot.org>
2024-12-18 07:53:32 +0530

    fix: show gkcore commit id if it is served from a git repo

--- src/gkcore/views/api_index.py
+++ src/gkcore/views/api_index.py
@@ -1,6 +1,7 @@
 from pyramid.config import os
 from pyramid.view import view_config, view_defaults
 import pkg_resources
+import subprocess
 
 
 @view_defaults(route_name="index")
@@ -10,8 +11,18 @@ class api_state(object):
 
     @view_config(request_method="GET", renderer="json")
     def main(self):
+
+        gkcore_version = os.getenv("GKCORE_VERSION")
+
+        if not gkcore_version:
+            try:
+                gkcore_version = subprocess.check_output(
+                    ["git", "rev-parse", "HEAD"]
+                ).strip().decode()[:8]
+            except FileNotFoundError:
+                gkcore_version = pkg_resources.require("gkcore")[0].version
+
         return {
             "gkstatus": 0,
-            "version": os.getenv("GKCORE_VERSION")
-            or pkg_resources.require("gkcore")[0].version,
+            "version": gkcore_version,
         }
