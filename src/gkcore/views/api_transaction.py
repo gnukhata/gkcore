ebeef6edf65b9ac8feab6fec6dabf56035d90873
Bady <bady@disroot.org>
2024-12-30 10:28:13 +0530

    refactor: use context manager for db connection in searchByDate

--- src/gkcore/views/api_transaction.py
+++ src/gkcore/views/api_transaction.py
@@ -1172,13 +1172,12 @@ class api_transaction(object):
         if authDetails["auth"] == False:
             return {"gkstatus": enumdict["UnauthorisedAccess"]}
         else:
-            try:
-                self.con = eng.connect()
+            with eng.connect() as con:
                 ur = getUserRole(authDetails["userid"], authDetails["orgcode"])
                 urole = ur["gkresult"]
                 fromDate = self.request.params["from"]
                 toDate = self.request.params["to"]
-                vouchersData = self.con.execute(
+                vouchersData = con.execute(
                     select(
                         [
                             vouchers.c.vouchercode,
@@ -1215,7 +1214,7 @@ class api_transaction(object):
                     finalCR = {}
                     tdr = 0.00
                     tcr = 0.00
-                    accname = self.con.execute(
+                    accname = con.execute(
                         select([accounts.c.accountname]).where(
                             accounts.c.accountcode == int(list(rawDr.keys())[0])
                         )
@@ -1232,7 +1231,7 @@ class api_transaction(object):
                     else:
                         finalDR[account["accountname"]] = rawDr[list(rawDr.keys())[0]]
 
-                    accname = self.con.execute(
+                    accname = con.execute(
                         select([accounts.c.accountname]).where(
                             accounts.c.accountcode == int(list(rawCr.keys())[0])
                         )
@@ -1268,15 +1267,11 @@ class api_transaction(object):
                             "status": voucher["lockflag"],
                         }
                     )
-                self.con.close()
                 return {
                     "gkstatus": enumdict["Success"],
                     "gkresult": voucherRecords,
                     "userrole": urole["userrole"],
                 }
-            except:
-                self.con.close()
-                return {"gkstatus": enumdict["ConnectionFailed"]}
 
     @view_config(
         request_method="GET", request_param="searchby=narration", renderer="json"
