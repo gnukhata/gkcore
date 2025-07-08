f24ea90c8c9f213a31737577fd30a77703421763
Kannan V M <kannan@disroot.org>
2025-07-08 09:13:37 +0530

    feat: add voucher number to brs response

--- src/gkcore/views/api_bankrecon.py
+++ src/gkcore/views/api_bankrecon.py
@@ -60,6 +60,7 @@ def get_bank_transactions(
                 bankrecon,
                 vouchers.c.voucherdate,
                 vouchers.c.narration,
+                vouchers.c.vouchernumber,
             ]
         )
         .where(
@@ -95,6 +96,7 @@ def get_bank_transactions(
             "date": datetime.strftime(record["voucherdate"], "%d-%m-%Y"),
             "particulars": account["accountname"],
             "vno": record["vouchercode"],
+            "voucher_no": record["vouchernumber"],
             "narration": record["narration"],
         }
         if record["entry_type"] == "Dr":
