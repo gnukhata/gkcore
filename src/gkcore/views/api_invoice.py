9ea412379234a084cc668201a341abc180f7f20f
Kannan V M <kannan@disroot.org>
2025-08-05 18:05:19 +0530

    feat: add suppport for cancelled invoice
    This will add a new parameter cancelled, if the value is 1. The API
    will list cancelled invoices.

--- src/gkcore/views/api_invoice.py
+++ src/gkcore/views/api_invoice.py
@@ -3541,25 +3541,34 @@ class api_invoice(object):
             return {"gkstatus": gkcore.enumdict["UnauthorisedAccess"]}
         else:
             with eng.connect() as con:
-                result = con.execute(
-                    select(
+                table = invoice
+                is_cancelled = False
+                if int(self.request.params.get("cancelled", 0)) == 1:
+                    table = invoicebin
+                    is_cancelled = True
+                inoutflag = self.request.params.get("inoutflag")
+
+                statement = select(
                         [
-                            invoice.c.invoiceno,
-                            invoice.c.invid,
-                            invoice.c.invoicedate,
-                            invoice.c.invoicetotal,
-                            invoice.c.immutable_data_id,
+                            table.c.invoiceno,
+                            table.c.invid,
+                            table.c.invoicedate,
+                            table.c.invoicetotal,
+                            table.c.immutable_data_id,
                         ]
-                    )
-                    .where(
+                    ).where(
                         and_(
-                            invoice.c.orgcode == authDetails["orgcode"],
-                            invoice.c.icflag == 3,
-                            invoice.c.inoutflag == self.request.params["inoutflag"],
+                            table.c.orgcode == authDetails["orgcode"],
+                            table.c.icflag == 3,
                         )
                     )
-                    .order_by(invoice.c.invoicedate)
-                )
+                if inoutflag:
+                    statement = statement.where(
+                        table.c.inoutflag == inoutflag
+                    )
+                result = con.execute(
+                    statement.order_by(table.c.invoicedate)
+                ).fetchall()
                 invoices = []
                 for row in result:
                     immutable_data = con.execute(
@@ -3568,6 +3577,7 @@ class api_invoice(object):
                     ).scalar()
                     invoices.append(
                         {
+                            "deletedFlag": is_cancelled,
                             "invoiceno": row["invoiceno"],
                             "invid": row["invid"],
                             "invoicedate": datetime.strftime(
